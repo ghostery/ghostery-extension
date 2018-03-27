d4df1b8de8b156c56f3872263d4e9aaa77d00820
Patrick Lawler <patricktlawler@gmail.com>
2018-03-27 15:01:56 -0400

    hot dog

---
+++ app/content-scripts/hotdog.js
@@ -0,0 +1,40 @@
+/**
+ * Ghostery HotDog
+ *
+ * This file injects the Ghostery HotDog into
+ * all pages except for ExtensionWeb and Platform pages
+ *
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2018 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+/**
+ * @namespace HotDogContentScript
+ */
+/* eslint no-use-before-define: 0 */
+
+import msgModule from './utils/msg';
+import { log } from '../../src/utils/common';
+// import closeIconImage from '../data-images/purple_box/closeIconImage';
+
+const msg = msgModule('hotdog');
+const { sendMessage } = msg;
+const { onMessage } = chrome.runtime;
+
+class HotDog {
+	constructor(win, doc) {
+		console.log('hot dog constructor');
+		this.win = win;
+		this.doc = doc;
+
+		console.log(win, window);
+		console.log(doc, document);
+	}
+}
+
+hotDog = new HotDog(window, document);
