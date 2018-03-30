96a0cecc29706f42a43b56b0f16cdab833d66cb2
Patrick Lawler <patricktlawler@gmail.com>
2018-03-30 17:32:46 -0400

    close circle

--- extension-manifest-v2/app/content-scripts/rewards/HotDog.jsx
+++ extension-manifest-v2/app/content-scripts/rewards/HotDog.jsx
@@ -11,19 +11,29 @@ class HotDog extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			closed: false,
 			rewardsCount: 1
 		};
 	}
 
+	close() {
+		this.setState({
+			closed: true
+		});
+	}
+
 	render() {
 		return (
-			<div className="ghostery-rewards-container ghostery-top ghostery-right ghostery-collapsed">
-				<div className="ghostery-box">
-					<div className={`ghostery-reward-text ${this.state.rewardsCount > 0 ? 'show' : 'hide'}`}>
-						{`${this.state.rewardsCount} ${this.state.rewardsCount > 1 ? t('rewards_text_plural') : t('rewards_text_single')}`}
-					</div>
-					<div className="ghostery-reward-close" />
-				</div>
+			<div>
+				{ this.state.closed !== true &&
+					<div className="ghostery-rewards-container ghostery-top ghostery-right ghostery-collapsed">
+						<div className="ghostery-box">
+							<div className={`ghostery-reward-text ${this.state.rewardsCount > 0 ? 'show' : 'hide'}`}>
+								{`${this.state.rewardsCount} ${this.state.rewardsCount > 1 ? t('rewards_text_plural') : t('rewards_text_single')}`}
+							</div>
+							<div className="ghostery-reward-close" onClick={this.close.bind(this)} />
+						</div>
+					</div>}
 			</div>
 		);
 	}
