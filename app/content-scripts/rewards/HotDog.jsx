f1c77feeaa5ee750a018f61bb39144598f1b1455
Patrick Lawler <patricktlawler@gmail.com>
2018-03-30 16:49:28 -0400

    rewards hot dog ui

--- app/content-scripts/rewards/HotDog.jsx
+++ app/content-scripts/rewards/HotDog.jsx
@@ -12,23 +12,17 @@ class HotDog extends Component {
 		super(props);
 		this.state = {
 			rewardsCount: 1
-		}
-		this.test();
-	}
-
-	test() {
-		setInterval(() => {
-			this.setState({
-				rewardsCount: ++this.state.rewardsCount
-			});
-		}, 3000);
+		};
 	}
 
 	render() {
 		return (
 			<div className="ghostery-rewards-container ghostery-top ghostery-right ghostery-collapsed">
 				<div className="ghostery-box">
-					{ this.state.rewardsCount } new Rewards were discovered!
+					<div className={`ghostery-reward-text ${this.state.rewardsCount > 0 ? 'show' : 'hide'}`}>
+						{`${this.state.rewardsCount} ${this.state.rewardsCount > 1 ? t('rewards_text_plural') : t('rewards_text_single')}`}
+					</div>
+					<div className="ghostery-reward-close" />
 				</div>
 			</div>
 		);
