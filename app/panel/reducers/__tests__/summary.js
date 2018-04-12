48b74b0f0b18312db2936661d883195f58ec5afc
Caleb Richelson <IAmThePan@users.noreply.github.com>
2018-04-12 10:52:36 -0400

    GH-862: Unit tests for the Panel (#31)
    * GH-862 Add unit tests for the Panel: Pause Button Snapshot, Summary Actions & Reducers
    
    * Update reducers to test for immutable state

---
+++ app/panel/reducers/__tests__/summary.js
@@ -0,0 +1,102 @@
+/**
+ * Test file for the Summary Reducer
+ *
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2018 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+import Immutable from 'seamless-immutable';
+import summaryReducer from '../summary';
+import {
+	GET_SUMMARY_DATA,
+	UPDATE_TRACKER_COUNTS,
+	UPDATE_GHOSTERY_PAUSED,
+	UPDATE_SITE_POLICY
+} from '../../constants/constants';
+
+// Copied from app/panel/reducers/summary.js
+const initialState = Immutable({
+	alertCounts: {
+		total: 0,
+	},
+	pageHost: '',
+	pageUrl: '',
+	paused_blocking: false,
+	siteNotScanned: false,
+	trackerCounts: {
+		allowed: 0,
+		blocked: 0,
+	},
+	tab_id: 0,
+});
+
+describe('app/panel/reducers/summary.js', () => {
+	test('initial state is correct', () => {
+		expect(summaryReducer(undefined, {})).toEqual(initialState);
+	});
+
+	test('reducer correctly handles GET_SUMMARY_DATA', () => {
+		const data = { test: true };
+		const action = { data, type: GET_SUMMARY_DATA };
+		const initState = Immutable({});
+
+		expect(summaryReducer(initState, action)).toEqual(data);
+	});
+
+	test('reducer correctly handles UPDATE_GHOSTERY_PAUSED', () => {
+		const data = { time: null, ghosteryPaused: true };
+		const action = { data, type: UPDATE_GHOSTERY_PAUSED };
+
+		const updatedState = Immutable.merge(initialState, {
+			paused_blocking: data.ghosteryPaused,
+			paused_blocking_timeout: data.time
+		});
+
+		expect(summaryReducer(undefined, action)).toEqual(updatedState);
+	});
+
+	test('reducer correctly handles UPDATE_SITE_POLICY', () => {
+		const data = { type: 'blacklist' };
+		const action = { data, type: UPDATE_SITE_POLICY };
+
+		const initState = Immutable({
+			pageHost: 'www.cnn.com',
+			sitePolicy: 2,
+			site_blacklist: [],
+			site_whitelist: ['cnn.com']
+		});
+
+		expect(summaryReducer(initState, action)).toEqual({
+			pageHost: 'www.cnn.com',
+			sitePolicy: 1,
+			site_blacklist: ['cnn.com'],
+			site_whitelist: []
+		});
+	});
+
+	test('reducer correctly handles UPDATE_TRACKER_COUNTS', () => {
+		const data = {
+			num_blocked: 3,
+			num_total: 8,
+			num_ss_blocked: 1,
+			num_ss_allowed: 2
+		};
+		const action = { data, type: UPDATE_TRACKER_COUNTS };
+		const initState = Immutable({});
+
+		expect(summaryReducer(initState, action)).toEqual({
+			trackerCounts: {
+				blocked: 3,
+				allowed: 5,
+				ssBlocked: 1,
+				ssAllowed: 2
+			}
+		});
+	});
+});
