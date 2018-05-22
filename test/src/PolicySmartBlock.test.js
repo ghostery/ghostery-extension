e7cc84ef70b08eb1029b08d9948e065a65d9cad1
Caleb Richelson <caleb.richelson@gmail.com>
2018-05-22 16:11:49 -0400

    GH-1084: update first party exception check and add some test cases

---
+++ test/src/PolicySmartBlock.test.js
@@ -0,0 +1,71 @@
+/**
+ * /src/classes/PolicySmartBlock.js Unit Tests
+ *
+ * Ghostery Browser Extension
+ * http://www.ghostery.com/
+ *
+ * Copyright 2018 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+
+import PolicySmartBlock from '../../src/classes/PolicySmartBlock';
+let policySmartBlock = new PolicySmartBlock();
+
+// Mock imports for dependencies
+jest.mock('../../src/classes/TabInfo', () => {});
+
+describe('src/classes/PolicySmartBlock.js', () => {
+	describe('PolicySmartBlock constructor tests', () => {
+		test('policy is constructed correctly', () => {
+			return expect(policySmartBlock.policy.constructor.name).toBe('Policy');
+		});
+		test('allowedCategoriesList is constructed correctly', () => {
+			const allowedCategoriesList = [
+				'essential',
+				'audio_video_player',
+				'comments',
+			];
+			return expect(policySmartBlock.allowedCategoriesList).toEqual(allowedCategoriesList);
+		});
+		test('allowedTypesList is constructed correctly', () => {
+			const allowedTypesList = [
+				'stylesheet',
+				'image',
+				'font',
+			];
+			return expect(policySmartBlock.allowedTypesList).toEqual(allowedTypesList);
+		});
+	});
+
+	describe('PolicySmartBlock isFirstPartyRequest tests', () => {
+		beforeAll(() => {
+			policySmartBlock.shouldCheck = jest.fn(() => true);
+		});
+
+		afterAll(() => {
+			policySmartBlock.mockClear();
+		});
+
+		test('PolicySmartBlock isFirstPartyRequest truthy assertion', () => {
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'example.com', 'example.com')).toBeTruthy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'www.example.com', 'example.com')).toBeTruthy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'example.com', 'www.example.com')).toBeTruthy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'www.example.com', 'www.example.com')).toBeTruthy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'sub.example.com', 'example.com')).toBeTruthy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'example.com', 'sub.example.com')).toBeTruthy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'sub1.sub2.sub3.example.com', 'sub2.sub3.example.com')).toBeTruthy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'www.ghostery.com', 'analytics.ghostery.com')).toBeTruthy();
+		});
+
+		test('PolicySmartBlock isFirstPartyRequest falsy assertion', () => {
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'sub1.example.com', 'sub2.example.com')).toBeFalsy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'sub1.sub2.example.com', 'sub2.sub1.example.com')).toBeFalsy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'example.com', 'test.com')).toBeFalsy();
+			expect(policySmartBlock.isFirstPartyRequest('tabId', 'www.example.com', 'www.test.com')).toBeFalsy();
+		});
+	});
+});
