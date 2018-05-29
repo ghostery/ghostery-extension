b01d8a2ef2011e420672c18b69b239ecdbe424bd
Patrick Lawler <patricktlawler@gmail.com>
2018-05-29 13:08:17 -0400

    remove unused test file

--- app/content-scripts/rewards/__tests__/HotDog.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router';
-import HotDog from '../HotDog';
-
-global.t = function (str) {
-	return str;
-};
