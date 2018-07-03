058943255ef69e86d7665507dbbb4e868d14306b
Serge Zarembsky <serge@ghostery.com>
2018-07-03 10:51:17 -0400

    Providing static info.

--- src/utils/perfutils.js
+++ src/utils/perfutils.js
@@ -1,23 +1,53 @@
 import * as utils from './utils';
+import conf from '../classes/Conf';
+import globals from '../classes/Globals';
 
-//ID of Ghostery-perf extension
-const RECEIVER_ID = "pdlmemohjlhncchohlaeifdmbjbngcld";
-let PORT = chrome.runtime.connect(RECEIVER_ID);
+// ID of Ghostery-perf extension
+const RECEIVER_ID = 'pdlmemohjlhncchohlaeifdmbjbngcld';
+let PORT;
+chrome.runtime.onMessageExternal.addListener(
+  function(request, sender, sendResponse) {
+    if (sender.id === RECEIVER_ID) {
+    	const {name} = request;
+    	if(name === 'perfReady') {
+    		sendResponse(_reportStaticInfo());
+    		PORT = chrome.runtime.connect(RECEIVER_ID);
+    		return true;
+    	}
+    }
+
+    return false;
+});
+
+function _reportStaticInfo() {
+	let gb;
+	let hasOnes = false;
+	let hasZeros = false;
+	Object.keys(conf.selected_app_ids).forEach(key => {
+		if(conf.selected_app_ids[key] === 1) {
+			hasOnes = true;
+		}
+		if(conf.selected_app_ids[key] === 0) {
+			hasZeros = true;
+		}
+	});
+	gb = (hasOnes && hasZeros) ? 2 : hasOnes ? 1 : 0;
+	return { name: 'perfInfo', 
+		ghostery_version: chrome.runtime.getManifest().version,
+		 
+		at: conf.enable_anti_tracking ? "1" : "0",
+		ab: conf.enable_ad_block ? "1" : "0",
+		sb: conf.enable_smart_block ? "1" : "0",
+		gb: gb,
+		gp: globals.SESSION.paused_blocking 
+	};
+}
 
 function _report(perfEntry, data) {
-	try {
-		if(!PORT) {
-			PORT = chrome.runtime.connect(RECEIVER_ID);
-			const report = {name: 'perfInfo', ghostery_version: chrome.runtime.getManifest().version};
-			PORT.postMessage(report);
-		} 
-	} catch(e) {
-		console.log("Failed to connect", e);
-		PORT = undefined;
-	}
-
-	if(PORT) {
-		const report = {name: 'perfData', startTime: perfEntry.startTime, duration: perfEntry.duration, handler: data.handler, page_url: data.page_url, url: data.url};
+	if (PORT) {
+		const report = {
+			name: 'perfData', startTime: perfEntry.startTime, duration: perfEntry.duration, handler: data.handler, page_url: data.page_url, url: data.url
+		};
 		PORT.postMessage(report);
 	}
 }
@@ -30,7 +60,7 @@ function _report(perfEntry, data) {
  * @param  {string} name name of the mark, should be unique
  */
 export function perfBegin(name) {
-	console.log("PERF BEGIN CALLED FOR", name);
+	console.log('PERF BEGIN CALLED FOR', name);
 	window.performance.mark(`BEGIN_${name}`);
 }
 /**
@@ -39,25 +69,25 @@ export function perfBegin(name) {
  * @return {number}      duration
  */
 export function perfEnd(name, data) {
-	let entry = {name: `BEGIN_${name}`, startTime: 0, duration: 0};
+	let entry = { name: `BEGIN_${name}`, startTime: 0, duration: 0 };
 	const p = window.performance.getEntriesByName(`BEGIN_${name}`, 'mark') || [];
 	if (p && p.length) {
-		console.log("BEGIN MARK WAS FOUND FOR", name);
+		console.log('BEGIN MARK WAS FOUND FOR', name);
 		window.performance.mark(`END_${name}`);
 		window.performance.measure(name, `BEGIN_${name}`, `END_${name}`);
 
 		const measures = window.performance.getEntriesByName(name, 'measure') || [];
 		if (measures && measures.length) {
 			entry = measures[0];
-			if(entry && data) {
+			if (entry && data) {
 				data.handler = name;
 				_report(entry, data);
-			} 
+			}
 		} else {
-			console.log("BAD MEASURE");
+			console.log('BAD MEASURE');
 		}
 	} else {
-		console.log("BEGIN MARK WAS NOT FOUND FOR", name);
+		console.log('BEGIN MARK WAS NOT FOUND FOR', name);
 	}
 	window.performance.clearMarks(`BEGIN_${name}`);
 	window.performance.clearMarks(`END_${name}`);
