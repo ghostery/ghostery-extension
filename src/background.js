5e547e2dc79215e8e59461829a3e826ecb99e1fa
José María Signanini <josesigna@gmail.com>
2018-07-16 19:56:08 -0400

    Refactor send validate account email.

--- src/background.js
+++ src/background.js
@@ -776,6 +776,16 @@ function onMessageHandler(request, sender, callback) {
 				log('FETCH USER ERROR');
 			});
 		return true;
+	} else if (name === 'account.sendValidateAccountEmail') {
+		account.sendValidateAccountEmail()
+			.then((success) => {
+				callback(success);
+			})
+			.catch((err) => {
+				callback(err);
+				log('sendValidateAccountEmail error', err);
+			});
+		return true;
 	} else if (name === 'update_database') {
 		checkLibraryVersion().then((result) => {
 			callback(result);
@@ -815,11 +825,6 @@ function onMessageHandler(request, sender, callback) {
 			} else {
 				callback(false);
 			}
-		});
-		return true;
-	} else if (name === 'sendVerificationEmail') {
-		account.sendVerificationEmail().then((result) => {
-			callback(result);
 		});
 		return true;
 	} else if (name === 'ping') {
