1f48fb5e495b7a5a308974e4d706030c0871f932
Christopher Tino <christopher_tino@yahoo.com>
2018-07-31 17:38:14 -0400

    Add down-caret to settings blocking header menu

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,16 @@
+/**
+ * JSON API
+ *
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2018 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
 export const _getJSONAPIErrorsObject = e => [{ title: e.message || '', detail: e.message || '', code: e.code || e.message || '' }];
 
 class Api {
