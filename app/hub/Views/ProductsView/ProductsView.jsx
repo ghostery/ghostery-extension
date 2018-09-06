489a69e264a3f6f71cf85701df6e30592c968419
Felix <fschmetz@student.ethz.ch>
2018-09-06 16:22:07 -0400

    Ghostery Hub - Supporter and Product Views (#183)
    * Product page, unfinished supporterview
    
    * updates to Products page
    
    * Supporter Page finished
    
    * Updates to Supporter and Rewards
    
    * rename images on Products page
    
    * Fix gradient button
    
    * Link up subscription status to Subscription view
    
    * Update comment
    
    * Update button and i18n side navigation

--- app/hub/Views/ProductsView/ProductsView.jsx
+++ app/hub/Views/ProductsView/ProductsView.jsx
@@ -9,8 +9,6 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0
- *
- * ToDo: Update this file.
  */
 
 import React, { Component } from 'react';
@@ -21,11 +19,10 @@ import React, { Component } from 'react';
  * @memberof HubComponents
  */
 class ProductsView extends Component {
-	/**
-	 * Lifecycle Event
-	 */
-	componentWillMount() {
-		const title = t('hub_products_title');
+	constructor(props) {
+		super(props);
+
+		const title = t('hub_products_page_title');
 		window.document.title = title;
 	}
 
@@ -35,8 +32,80 @@ class ProductsView extends Component {
 	 */
 	render() {
 		return (
-			<div>
-				{t('hub_products_title')}
+			<div className="ProductsView">
+				<div className="ProductsView--rowPaddingTop row align-center-middle">
+					<div className="columns text-center">
+						<img
+							className="ProductsView__mobileScreenshots"
+							src="/app/images/hub/products/product-mobile-browser.png"
+							alt={t('hub_products_main_promo_title')}
+						/>
+					</div>
+					<div className="columns">
+						<h1 className="ProductsView__headerTitle">
+							{t('hub_products_header_title')}
+						</h1>
+						<div className="ProductsView__headerDescription">
+							{t('hub_products_header_description')}
+						</div>
+						<div className="ProductsView__mainPromoTitle" >
+							{t('hub_products_main_promo_title')}
+						</div>
+						<div className="ProductsView__mainPromoDescription" >
+							{t('hub_products_main_promo_description')}
+						</div>
+						<div className="ProductsView__storeImageContainer">
+							<a href="https://itunes.apple.com/us/app/ghostery-privacy-browser/id472789016?mt=8" target="_blank" rel="noopener noreferrer">
+								<img
+									className="ProductsView__imageAppStore"
+									src="/app/images/hub/products/appstore-icon.png"
+								/>
+							</a>
+							<a href="https://play.google.com/store/apps/details?id=com.ghostery.android.ghostery" target="_blank" rel="noopener noreferrer">
+								<img
+									className="ProductsView__imagePlayStore"
+									src="/app/images/hub/products/googleplay-icon.png"
+								/>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="ProductsView--rowPaddingTop row align-center">
+					<div className="ProductsView__secondaryPromo columns">
+						<img
+							className="ProductsView__secondaryPromoImage"
+							src="/app/images/hub/products/product-new-tab.png"
+							alt={t('hub_products_second_promo_title')}
+						/>
+						<div className="ProductsView__secondaryPromoTitle">
+							{t('hub_products_second_promo_title')}
+						</div>
+						<div className="ProductsView__secondaryPromoDescription">
+							{t('hub_products_second_promo_description')}
+						</div>
+					</div>
+					<div className="ProductsView__secondaryPromo columns">
+						<img
+							className="ProductsView__secondaryPromoImage"
+							src="/app/images/hub/products/product-ghostery-lite.png"
+							alt={t('hub_products_third_promo_title')}
+						/>
+						<div className="ProductsView__secondaryPromoTitle">
+							{t('hub_products_third_promo_title')}
+						</div>
+						<div className="ProductsView__secondaryPromoDescription">
+							{t('hub_products_third_promo_description')}
+						</div>
+						<div className="ProductsView__storeImageContainer">
+							<a href="https://itunes.apple.com/us/app/ghostery-privacy-browser/id472789016?mt=8" target="_blank" rel="noopener noreferrer">
+								<img
+									className="ProductsView__imageAppStore"
+									src="/app/images/hub/products/appstore-icon.png"
+								/>
+							</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
