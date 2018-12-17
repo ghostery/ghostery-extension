2ff875b439d477e5a9ba14f8600d7425ed9e641c
Serge Zarembsky <serge@ghostery.com>
2018-12-17 18:05:49 -0500

    Fix for GH-1498

--- app/panel/components/Subscription/SubscriptionInfo.jsx
+++ app/panel/components/Subscription/SubscriptionInfo.jsx
@@ -39,7 +39,7 @@ function _handleManageClick() {
  */
 const SubscriptionInfo = (props) => {
 	const {
-		active, plan_amount, plan_interval, charge_date, plan_ends, loading
+		data_error, active, plan_amount, plan_interval, charge_date, plan_ends, loading
 	} = props.subscriptionData;
 	const subscriptionExpiration = (plan_ends > 1) ? t('subscription_days_left', plan_ends.toString()) : t('subscription_one_day_left');
 	return (
@@ -47,46 +47,56 @@ const SubscriptionInfo = (props) => {
 			<div className="row">
 				<div className="columns column-subscription">
 					<h1>{ t('subscription_info_title') }</h1>
-					{loading ? (
+					{ loading ? (
 						<div className="loading" />
 					) : (
-						<div>
-							<div className="status-row">
-								<span className="status-label">{`${t('subscription_plan')}: `}</span>
-								<span className="status-value blue">{`${plan_amount} / ${plan_interval}`}</span>
-							</div>
-							{ plan_ends ? (
-								<div className="status-row">
-									<span className="status-label">{`${t('subscription_status')}: `}</span>
-									<span className="status-value red">{ subscriptionExpiration }</span>
-									<div style={{ marginTop: '20px' }}>
-										<span className="status-value blue resubscribe" onClick={_handleResubscribeClick}>{ t('subscription_resubscribe') }</span>
-									</div>
+						data_error ? (
+							<div>
+								<div className="data-retrieval-error">{t('subscription_data_error')}</div>
+								<div className="manage-row">
+									<div className="manage-icon" />
+									<span className="manage-link" onClick={_handleManageClick}>{t('subscription_manage')}</span>
 								</div>
-							) : (
-								<div>
+							</div>
+						) : (
+							<div>
+								<div className="status-row">
+									<span className="status-label">{`${t('subscription_plan')}: `}</span>
+									<span className="status-value blue">{`${plan_amount} / ${plan_interval}`}</span>
+								</div>
+								{ plan_ends ? (
 									<div className="status-row">
 										<span className="status-label">{`${t('subscription_status')}: `}</span>
-										<span className="status-value blue">{active ? t('subscription_active') : t('subscription_inactive') }</span>
+										<span className="status-value red">{ subscriptionExpiration }</span>
+										<div style={{ marginTop: '20px' }}>
+											<span className="status-value blue resubscribe" onClick={_handleResubscribeClick}>{ t('subscription_resubscribe') }</span>
+										</div>
 									</div>
-									<div className="status-row">
-										<span className="status-label">{active ? `${t('subscription_charge_date')}: ` : `${t('subscription_expired')}: `}</span>
-										<span className="status-value blue">{charge_date}</span>
+								) : (
+									<div>
+										<div className="status-row">
+											<span className="status-label">{`${t('subscription_status')}: `}</span>
+											<span className="status-value blue">{active ? t('subscription_active') : t('subscription_inactive') }</span>
+										</div>
+										<div className="status-row">
+											<span className="status-label">{active ? `${t('subscription_charge_date')}: ` : `${t('subscription_expired')}: `}</span>
+											<span className="status-value blue">{charge_date}</span>
+										</div>
+										<div className="list-row">
+											<ul>
+												<li className="list-item">{t('subscription_midnight_theme')}</li>
+												<li className="list-item">{t('subscription_priority_support')}</li>
+												{/* <li className="list-item">{t('subscription_tracker_stats')}</li> */}
+											</ul>
+										</div>
+										<div className="manage-row">
+											<div className="manage-icon" />
+											<span className="manage-link" onClick={_handleManageClick}>{t('subscription_manage')}</span>
+										</div>
 									</div>
-									<div className="list-row">
-										<ul>
-											<li className="list-item">{t('subscription_midnight_theme')}</li>
-											<li className="list-item">{t('subscription_priority_support')}</li>
-											{/* <li className="list-item">{t('subscription_tracker_stats')}</li> */}
-										</ul>
-									</div>
-									<div className="manage-row">
-										<div className="manage-icon" />
-										<span className="manage-link" onClick={_handleManageClick}>{t('subscription_manage')}</span>
-									</div>
-								</div>
-							)}
-						</div>
+								)}
+							</div>
+						)
 					)}
 				</div>
 			</div>
@@ -95,3 +105,4 @@ const SubscriptionInfo = (props) => {
 };
 
 export default SubscriptionInfo;
+
