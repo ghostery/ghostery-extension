804ddacfc5551fa39140bc7196e5a0ef82a7accb
José María Signanini <josesigna@gmail.com>
2018-12-31 14:23:07 -0500

    GIS-101: fix web vs extension log in sync issues.

--- src/background.js
+++ src/background.js
@@ -277,29 +277,45 @@ function getSiteData() {
  * @param  {string}		tab_url 	tab url
  */
 function handleAccountPages(name, callback) {
-	if (name === 'accountPageLoaded') {
-		if (conf.account === null) {
-			account._getUserIDFromCookie().then((userID) => {
-				account._setAccountInfo(userID);
-			})
-				.then(account.getUser)
-				.then(account.getUserSettings)
-				.catch((err) => {
-					log('handleAccountPages error', err);
-				});
-		}
-	} else if (name === 'account.logout') {
-		account.logout()
-			.then((response) => {
-				callback(response);
-			})
-			.catch((err) => {
-				log('LOGOUT ERROR', err);
-				callback(err);
-			});
-		return true;
+	switch (name) {
+		case 'accountPage.login':
+		case 'accountPage.register':
+		case 'accountPageLoaded': // legacy
+			if (conf.account === null) {
+				account._getUserIDFromCookie()
+					.then((userID) => {
+						account._setAccountInfo(userID);
+					})
+					.then(account.getUser)
+					.then(account.getUserSettings)
+					.then(account.getUserSubscriptionData)
+					.then(() => callback())
+					.catch((err) => {
+						callback(err);
+						log('handleAccountPages error', err);
+					});
+			}
+			return true;
+		case 'accountPage.getUser':
+			account.getUser()
+				.then(data => callback(data))
+				.catch(err => callback(err));
+			return true;
+		case 'accountPage.getUserSubscriptionData':
+			account.getUserSubscriptionData()
+				.then(data => callback(data))
+				.catch(err => callback(err));
+			return true;
+		case 'accountPage.logout':
+		case 'account.logout': // legacy
+			account.logout()
+				.then(data => callback(data))
+				.catch(err => callback(err));
+			return true;
+
+		default:
+			return false;
 	}
-	return false;
 }
 
 /**
