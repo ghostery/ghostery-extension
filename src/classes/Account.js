ffc80282aeb8956044711e4216703f140952ba0b
José María Signanini <josesigna@gmail.com>
2018-12-31 13:13:10 -0500

    Get user id from cookie.

--- src/classes/Account.js
+++ src/classes/Account.js
@@ -419,7 +419,14 @@ class Account {
 	_getUserID = () => (
 		new Promise((resolve, reject) => {
 			if (conf.account === null) {
-				return reject(new Error('_getUserID() Not logged in'));
+				return this._getUserIDFromCookie()
+					.then((userID) => {
+						this._setAccountInfo(userID);
+						resolve(conf.account.userID);
+					})
+					.catch((err) => {
+						reject(new Error('_getUserID() Not logged in'));
+					});
 			}
 			return resolve(conf.account.userID);
 		})
@@ -458,7 +465,6 @@ class Account {
 	}
 
 	_setAccountUserInfo = (user) => {
-		console.log('here _setAccountUserInfo', user);
 		conf.account.user = user;
 		dispatcher.trigger('conf.save.account');
 	}
