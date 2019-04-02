7f602af333135695d99b5b60e61f44dbc0d16f39
Christopher Tino <christopher_tino@yahoo.com>
2019-04-02 11:07:14 -0400

    revert falsey check on account reducer

--- app/Account/AccountReducer.js
+++ app/Account/AccountReducer.js
@@ -34,7 +34,7 @@ export default (state = initialState, action) => {
 	switch (action.type) {
 		case UPDATE_PANEL_DATA: {
 			const { account } = action.data;
-			if (!account) {
+			if (account === null) {
 				return Object.assign({}, initialState);
 			}
 			const {
