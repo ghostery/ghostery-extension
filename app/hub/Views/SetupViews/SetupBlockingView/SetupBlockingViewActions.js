f5746ee264f40dc2be7464f44d8e77f99434430e
Christopher Tino <4699516+christophertino@users.noreply.github.com>
2019-04-04 14:56:56 -0400

    update copyright year (#370)

--- app/hub/Views/SetupViews/SetupBlockingView/SetupBlockingViewActions.js
+++ app/hub/Views/SetupViews/SetupBlockingView/SetupBlockingViewActions.js
@@ -4,7 +4,7 @@
  * Ghostery Browser Extension
  * https://www.ghostery.com/
  *
- * Copyright 2018 Ghostery, Inc. All rights reserved.
+ * Copyright 2019 Ghostery, Inc. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
