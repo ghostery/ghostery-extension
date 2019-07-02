60d14dbb56d75182bf2d47de89744e7f73ab9dc7
Christopher Tino <christopher_tino@yahoo.com>
2019-07-02 14:54:42 -0400

    bump browser core version

--- extension-manifest-v2/src/classes/PanelData.js
+++ extension-manifest-v2/src/classes/PanelData.js
@@ -232,9 +232,8 @@ class PanelData {
 	 * Invoked in EventHandlers#onBeforeRequest when a new bug has been found
 	 * Sends updated data to the panel and blocking and/or summary components
 	 */
-	updatePanelUI = throttle(this._updatePanelUI.bind(this), 600, { leading: true, trailing: true });
+	updatePanelUI = throttle(this._updatePanelUI.bind(this), 600, { leading: true, trailing: true }); // matches donut redraw throttling
 
-	// matches donut redraw throttling
 	_updatePanelUI() {
 		if (!this._panelPort || !this._activeTab) { return; }
 
