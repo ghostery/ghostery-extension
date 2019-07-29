f6225b83412b96b16304347f784f7d5441a50fb9
ilyazarembsky <wlycdgrfromflatiron@gmail.com>
2019-07-29 12:47:12 -0400

    Add doc header to I18nWithLink

--- app/shared-components/I18nWithLink/I18nWithLink.jsx
+++ app/shared-components/I18nWithLink/I18nWithLink.jsx
@@ -1,3 +1,15 @@
+/**
+ * I18nWithLink Component
+ *
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2019 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
 import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 
