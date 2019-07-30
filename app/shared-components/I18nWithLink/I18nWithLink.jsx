ff04783fdb1cebe1d581fa258fd8d978b4548dfd
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2019-07-30 17:26:22 -0400

    Gh 1767/bugfix/target blank links crash opera (#432)
    * Specify default for optional cssClasses prop to I18nWithLink

--- app/shared-components/I18nWithLink/I18nWithLink.jsx
+++ app/shared-components/I18nWithLink/I18nWithLink.jsx
@@ -38,9 +38,10 @@ class I18nWithLink extends Component {
 	}
 
 	render() {
-		const { value } = this.props;
+		const { value, cssClasses } = this.props;
+
 		return (
-			<span ref={this.containerRef} dangerouslySetInnerHTML={{ __html: t(value) }} />
+			<span className={cssClasses} ref={this.containerRef} dangerouslySetInnerHTML={{ __html: t(value) }} />
 		);
 	}
 }
@@ -48,5 +49,10 @@ class I18nWithLink extends Component {
 export default I18nWithLink;
 
 I18nWithLink.propTypes = {
+	cssClasses: PropTypes.string,
 	value: PropTypes.string.isRequired,
+};
+
+I18nWithLink.defaultProps = {
+	cssClasses: '',
 };
