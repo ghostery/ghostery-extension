31f10f19eb9c6323c3cf1d619a2dcd9ec2092d2c
Christopher Tino <christopher_tino@yahoo.com>
2019-09-04 12:43:22 -0400

    disable overlay search

--- src/background.js
+++ src/background.js
@@ -1209,13 +1209,13 @@ function setupABTest() {
 		prefs.set('attrackBloomFilter', false);
 	}
 	// overlay search AB test
-	if (abtest.hasTest('overlay_search')) {
-		cliqz.enableModule('search');
-		cliqz.enableModule('overlay');
-	} else {
-		cliqz.disableModule('search');
-		cliqz.disableModule('overlay');
-	}
+	// if (abtest.hasTest('overlay_search')) {
+	// 	cliqz.enableModule('search');
+	// 	cliqz.enableModule('overlay');
+	// } else {
+	// 	cliqz.disableModule('search');
+	// 	cliqz.disableModule('overlay');
+	// }
 }
 
 /**
--- tools/merge-locales.sh
+++ tools/merge-locales.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
+#
+# Merge Cliqz language strings into Ghostery _locales
+#
+# Ghostery Browser Extension
+# http://www.ghostery.com/
+#
+# Copyright 2019 Ghostery, Inc. All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0
+
 for lang in `ls ./cliqz/static/locale`
 do
 	# filter out only language folder
 	if [ ! -d "./cliqz/static/locale/$lang" ]; then
 		continue;
 	fi
-	# filter out language folders supported by ghostery
 
+	# filter out language folders supported by ghostery
 	if [ ! -d "_locales/$lang" ]; then
 		continue;
 	fi
