3cff5eb1ee9b6554e838d904b42ec5478f386588
Ben <benjamin.strumeyer@gmail.com>
2020-02-11 13:42:36 -0500

    Refactor matchesWildcardOrRegex and remove escape-strings-regex dependency

--- app/panel/components/Settings/TrustAndRestrict.jsx
+++ app/panel/components/Settings/TrustAndRestrict.jsx
@@ -89,9 +89,6 @@ class TrustAndRestrict extends React.Component {
 	 * if it has been already added to the opposite list. Displays appropriate warnings.
 	 */
 	addSite() {
-		// TODO: Implement paygating, use this regex for validating free users who don't have regex/wildcard ability
-		// from node-validator
-		const isValidUrlRegex = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 		let pageHost;
 		let list;
 		let listType;
