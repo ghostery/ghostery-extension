1e249b12842f1fc098d4496d6b9505e1864fcf13
Ben <benjamin.strumeyer@gmail.com>
2020-04-16 11:14:24 -0400

    Delete functions from app/panel/utils. Remove console log

--- app/Account/AccountActions.js
+++ app/Account/AccountActions.js
@@ -166,7 +166,6 @@ export const logout = () => dispatch => (
 );
 
 export const resetPassword = email => dispatch => (
-	// console.log('resetting password...');
 	sendMessageInPromise('account.resetPassword', { email })
 		.then((res) => {
 			const { errors } = res;
