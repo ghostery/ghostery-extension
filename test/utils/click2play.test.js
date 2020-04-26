dcbcc8df6ec2bb0e706d0b5bb0e6abb877c4feb6
Christopher Tino <christopher_tino@yahoo.com>
2020-04-26 18:03:10 -0400

    add units tests for allowAllwaysC2P

--- test/utils/click2play.test.js
+++ test/utils/click2play.test.js
@@ -11,10 +11,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
 
-import { buildC2P, buildRedirectC2P } from '../../src/utils/click2play';
+import { buildC2P, buildRedirectC2P, allowAllwaysC2P } from '../../src/utils/click2play';
 import tabInfo from '../../src/classes/TabInfo';
 import Policy from '../../src/classes/Policy';
 import globals from '../../src/classes/Globals';
+import conf from '../../src/classes/Conf';
 import c2p_tpl from '../../app/templates/click2play.html';
 import * as utils from '../../src/utils/utils';
 
@@ -79,6 +80,19 @@ jest.mock('../../src/classes/BugDb', () => ({
 			},
 		}
 	}
+}));
+jest.mock('../../src/classes/Conf', () => ({
+		selected_app_ids: {
+			15: 1,
+			41: 1,
+		},
+		site_specific_blocks: {
+			'www.ghostery.com': [15, 100],
+			'www.cnn.com': [41],
+		},
+		site_specific_unblocks: {
+			'www.cnn.com': [15, 50],
+		},
 }));
 
 // Mock TabInfo data
@@ -181,6 +195,31 @@ describe('src/utils/click2play.js', () => {
 			buildRedirectC2P(REDIRECT_MAP.get(100), 464);
 			expect(globals.BLOCKED_REDIRECT_DATA.app_id).toBe(464);
 			expect(globals.BLOCKED_REDIRECT_DATA.url).toBe('https://fake-redirect.com/');
+		});
+	});
+
+	describe('testing allowAllwaysC2P()', () => {
+		test('app_id is removed from selected_app_ids', () => {
+			allowAllwaysC2P(15, 'www.espn.com');
+			expect(conf.selected_app_ids).not.toHaveProperty('15');
+			expect(conf.selected_app_ids).toMatchObject({41: 1});
+		});
+
+		test('app_id is removed from site_specific_blocks', () => {
+			allowAllwaysC2P(15, 'www.ghostery.com');
+			expect(conf.site_specific_blocks['www.ghostery.com']).toEqual(expect.not.arrayContaining([15]));
+			expect(conf.site_specific_blocks['www.ghostery.com']).toEqual(expect.arrayContaining([100]));
+		});
+
+		test('app_id is added to site_specific_unblocks', () => {
+			allowAllwaysC2P(41, 'www.cnn.com');
+			expect(conf.site_specific_unblocks['www.cnn.com']).toEqual(expect.arrayContaining([15,50,41]));
+			// Check results from preceding tests
+			expect(conf.site_specific_unblocks['www.espn.com']).toEqual(expect.arrayContaining([15]));
+			expect(conf.site_specific_unblocks['www.ghostery.com']).toEqual(expect.arrayContaining([15]));
+			// Check removal from selected_app_ids and site_specific_blocks as well
+			expect(conf.selected_app_ids).not.toHaveProperty('41');
+			expect(conf.site_specific_blocks['www.cnn.com']).toEqual(expect.not.arrayContaining([41]));
 		});
 	});
 });
