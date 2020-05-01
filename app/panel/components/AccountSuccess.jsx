f89d00617f6b0dc6a89d442e718633f25a3e126c
Ben <benjamin.strumeyer@gmail.com>
2020-05-01 09:54:31 -0400

    Update account creation panel with premium benefits

--- app/panel/components/AccountSuccess.jsx
+++ app/panel/components/AccountSuccess.jsx
@@ -12,7 +12,8 @@
  */
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 /**
  * @class Implement Account Success component which opens
  * in place of Sign In view on successful signing.
@@ -21,14 +22,18 @@ import { Link } from 'react-router-dom';
 const AccountSuccess = (props) => ( // eslint-disable-line arrow-parens
 	<div id="signin-success-panel">
 		<div className="row align-center">
-			<div className="small-7 columns text-center">
-				<h3>{ t('panel_signin_success_title') }</h3>
-				<div className="big-ghosty" />
-				<p className="small-and-pale">{ t('panel_signin_success') }</p>
+			<div className="small-11 columns text-center">
+				<h3 className="signin-success-title">{ t('panel_signin_success_title') }</h3>
+				<p>{ t('panel_signin_success') }</p>
 				<h3 className="signin-success-email">{ props.email }</h3>
-				<Link to={(props.is_expert ? '/detail' : '/')} id="view-trackers-button" className="button">
-					{ t('panel_view_trackers') }
-				</Link>
+				<div className="premium-sparkles-icon" />
+				<h4 className="signin-premium-benefits">{ t('panel_signin_premium_benefits') }</h4>
+				<a className="learn-more-button" href="https://ghostery.com/midnight?utm_source=gbe&utm_campaign=in_app_account_creation_success" alt={t('learn_more')} target="_blank" rel="noopener noreferrer">
+					{t('learn_more')}
+				</a>
+				<NavLink className="no-thanks-maybe-later text-center" to="/">
+					{t('subscribe_pitch_no_thanks')}
+				</NavLink>
 			</div>
 		</div>
 	</div>
