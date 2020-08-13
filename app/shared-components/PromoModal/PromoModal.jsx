874760b0aa202d04b272db2dea3a2c0ddca20cc0
Ben <benjamin.strumeyer@gmail.com>
2020-08-13 16:36:00 -0400

    Use GHOSTERY_WEBSITE_BASE_URL in insights CTA

--- app/shared-components/PromoModal/PromoModal.jsx
+++ app/shared-components/PromoModal/PromoModal.jsx
@@ -76,7 +76,7 @@ class PromoModal extends React.Component {
 				break;
 			case INSIGHTS:
 				sendMessage('ping', 'promo_modals_insights_upgrade_cta');
-				url = `${CHECKOUT_BASE_URL}/insights/?utm_source=gbe&utm_campaign=${utm_campaign}`;
+				url = `${GHOSTERY_WEBSITE_BASE_URL}/insights/?utm_source=gbe&utm_campaign=${utm_campaign}`;
 				break;
 			default:
 		}
