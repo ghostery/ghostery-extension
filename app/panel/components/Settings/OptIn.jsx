c563a1d49f2f9289bd32c7d6d4d0128c103945b9
wlycdgr <ilya.zarembsky@gmail.com>
2020-09-22 16:35:26 -0400

    Clean up OptIn a bit in preparation for adding A/B test checkbox

--- app/panel/components/Settings/OptIn.jsx
+++ app/panel/components/Settings/OptIn.jsx
@@ -18,62 +18,76 @@ import globals from '../../../../src/classes/Globals';
 const { IS_CLIQZ, BROWSER_INFO } = globals;
 const IS_ANDROID = (BROWSER_INFO.os === 'android');
 
+const TOOLTIP_SVG_FILEPATH = '../../app/images/panel/icon-information-tooltip.svg';
+
 /**
  * @class Implement Opt In subview as a React component.
  * The view opens from the left-side menu of the main Settings view.
  * It invites user to opt in for telemetry options, human web and offers
  * @memberOf SettingsComponents
  */
-const OptIn = ({ settingsData, toggleCheckbox }) => (
-	<div className="s-tabs-panel">
-		<div className="row">
-			<div className="columns">
-				<h3>{ t('settings_support_ghostery') }</h3>
-				<h5>
-					{ t('settings_support_ghostery_by') }
-					:
-				</h5>
-				<div className="s-option-group">
-					<div className="s-square-checkbox">
-						<input type="checkbox" id="settings-share-usage" name="enable_metrics" defaultChecked={settingsData.enable_metrics} onClick={toggleCheckbox} />
-						<label htmlFor="settings-share-usage">
-							<span>{ t('settings_share_usage') }</span>
-						</label>
-						<div className="s-tooltip-down" data-g-tooltip={t('settings_share_usage_tooltip')}>
-							<img src="../../app/images/panel/icon-information-tooltip.svg" className="s-question" />
+const OptIn = ({ settingsData, toggleCheckbox }) => {
+	const checkbox = (id, name) => (
+		<input
+			type="checkbox"
+			id={id}
+			name={name}
+			defaultChecked={settingsData[name]}
+			onClick={toggleCheckbox}
+		/>
+	);
+
+	const labelFor = inputId => (
+		<label htmlFor={inputId}>
+			<span>{t(inputId.replaceAll('-', '_'))}</span>
+		</label>
+	);
+
+	return (
+		<div className="s-tabs-panel">
+			<div className="row">
+				<div className="columns">
+					<h3>{t('settings_support_ghostery')}</h3>
+					<h5>
+						{t('settings_support_ghostery_by')}
+						:
+					</h5>
+					<div className="s-option-group">
+						<div className="s-square-checkbox">
+							{checkbox('settings-share-usage', 'enable_metrics')}
+							{labelFor('settings-share-usage')}
+							<div className="s-tooltip-down" data-g-tooltip={t('settings_share_usage_tooltip')}>
+								<img src={TOOLTIP_SVG_FILEPATH} className="s-question" />
+							</div>
 						</div>
 					</div>
+					{!IS_CLIQZ && (
+						<div className="s-option-group" id="human-web-section">
+							<div className="s-square-checkbox">
+								{checkbox('settings-share-usage', 'enable_human_web')}
+								{labelFor('settings-share-human-web')}
+								<div className="s-tooltip-up" data-g-tooltip={t('settings_human_web_tooltip')}>
+									<img src={TOOLTIP_SVG_FILEPATH} className="s-question" />
+								</div>
+							</div>
+						</div>
+					)}
+					{!IS_CLIQZ && !IS_ANDROID && (
+						<div className="s-option-group" id="offers-section">
+							<div className="s-square-checkbox">
+								{checkbox('settings-allow-offers', 'enable_offers')}
+								{labelFor('settings-allow-offers')}
+								<div className="s-tooltip-up" data-g-tooltip={t('settings_offers_tooltip')}>
+									<img src={TOOLTIP_SVG_FILEPATH} className="s-question" />
+								</div>
+							</div>
+						</div>
+					)}
 				</div>
-				{!IS_CLIQZ && (
-					<div className="s-option-group" id="human-web-section">
-						<div className="s-square-checkbox">
-							<input type="checkbox" id="settings-share-human-web" name="enable_human_web" defaultChecked={settingsData.enable_human_web} onClick={toggleCheckbox} />
-							<label htmlFor="settings-share-human-web">
-								<span>{ t('settings_share_human_web') }</span>
-							</label>
-							<div className="s-tooltip-up" data-g-tooltip={t('settings_human_web_tooltip')}>
-								<img src="../../app/images/panel/icon-information-tooltip.svg" className="s-question" />
-							</div>
-						</div>
-					</div>
-				)}
-				{!IS_CLIQZ && !IS_ANDROID && (
-					<div className="s-option-group" id="offers-section">
-						<div className="s-square-checkbox">
-							<input type="checkbox" id="settings-allow-offers" name="enable_offers" defaultChecked={settingsData.enable_offers} onClick={toggleCheckbox} />
-							<label htmlFor="settings-allow-offers">
-								<span>{ t('settings_allow_offers') }</span>
-							</label>
-							<div className="s-tooltip-up" data-g-tooltip={t('settings_offers_tooltip')}>
-								<img src="../../app/images/panel/icon-information-tooltip.svg" className="s-question" />
-							</div>
-						</div>
-					</div>
-				)}
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 OptIn.propTypes = {
 	toggleCheckbox: PropTypes.func.isRequired,
