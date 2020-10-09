691e7cf7b432347fc8b5276f8e573dce3af6e36d
Ilya Zarembsky <ilya.zarembsky@gmail.com>
2020-10-09 09:16:32 -0400

    Update Learn More link in the upgrade banner promo modal

--- app/panel/components/Subscribe.jsx
+++ app/panel/components/Subscribe.jsx
@@ -38,7 +38,7 @@ const Subscribe = ({ match }) => {
 			<div className="pitch-container">
 				<span className="pitch-text" dangerouslySetInnerHTML={{ __html: t('subscribe_pitch') }} />
 			</div>
-			<PanelToTabLink href="https://www.ghostery.com/products/plus?utm_source=gbe&utm_campaign=in_app_plus_lm">
+			<PanelToTabLink href="https://www.ghostery.com/pricing/">
 				<span className="pitch-learn-more">{t('subscribe_pitch_learn_more')}</span>
 			</PanelToTabLink>
 			<div>
