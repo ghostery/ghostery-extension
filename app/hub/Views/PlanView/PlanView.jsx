6e38e7c68b2725d3b39a4b1eac71eb7b609fb5cd
Ben <benjamin.strumeyer@gmail.com>
2020-11-25 08:59:09 -0500

    Add value prop list to cards

--- app/hub/Views/PlanView/PlanView.jsx
+++ app/hub/Views/PlanView/PlanView.jsx
@@ -21,92 +21,135 @@ import globals from '../../../../src/classes/Globals';
 const searchPromo = () => (
 	<div className="PlanView__searchPromoContainer">
 		<div className="PlanView__searchLogo" />
-		<div className="PlanView__adFree">{ t('hub_plan_view_ad_free') }</div>
-		<div className="PlanView__adFreePromo">{ t('hub_plan_view_ad_free_promo') }</div>
-		<div className="PlanView__adFreePromoDescription">{ t('hub_plan_view_ad_free_promo_description') }</div>
+		<div className="PlanView__adFree">{ t('hub_plan_ad_free_with_ghostery_plus_subscription') }</div>
+		<div className="PlanView__adFreePromo">{ t('hub_plan_ad_free_promo') }</div>
+		<div className="PlanView__adFreePromoDescription">{ t('hub_plan_ad_free_promo_description') }</div>
 	</div>
 );
 
 const basicCard = () => (
-	<div className="card-outer">
-		<div className="card basic" data-equalizer-watch>
+	<div className="PlanView__cardOuter">
+		<div className="PlanView__card basic" data-equalizer-watch>
 			<div className="card-header-background-free" />
 			<div className="ghostery-free-image-container">
 				<div className="ghostery-free-image text-center" title="Ghostery Free" alt="Ghostery Free" />
 			</div>
 			<h2>Ghostery</h2>
-			<div className="price">
-				<p className="price-blue-alt font-size-36">{t('hub_upgrade_plan_free')}</p>
+			<div className="PlanView__price">
+				<p className="PlanView__price-blue-alt font-size-36">{t('hub_upgrade_plan_free')}</p>
 			</div>
-			<NavLink className="button primary already-protected" to="/home" title="Already Protected">
-				{t('hub_upgrade_already_protected')}
-			</NavLink>
 			<p className="card-sub-header"><strong>{t('hub_upgrade_basic_protection')}</strong></p>
-			<p className="card-sub-copy">
-				<span className="check blue" />
-				{t('hub_upgrade_basic_browser_protection')}
-			</p>
+			<div className="PlanView__valuePropList basic">
+				<div className="PlanView__cardSubCopy">
+					{t('hub_plan_private_search')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					{t('hub_plan_tracker_protection')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					{t('hub_plan_speedy_page_loads')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					{t('hub_plan_intelligence_technology')}
+				</div>
+			</div>
 		</div>
 	</div>
 );
 
-const plusCard = mobileView => (
-	<div className="card-outer">
-		<div className="card plus" data-equalizer-watch>
+const plusCard = () => (
+	<div className="PlanView__cardOuter">
+		<div className="PlanView__card plus" data-equalizer-watch>
 			<div className="ghostery-plus-image-container">
 				<div className="ghostery-plus-image" title="Ghostery Plus" alt="Ghostery Plus" />
 			</div>
 			<h2>Ghostery Plus</h2>
-			<div className="price">
+			<div className="PlanView__price">
 				<Fragment>
-					<p className="price-gold font-size-36">$4.99</p>
-					<p className="price-gold sub-text font-size-12">{t('per_month')}</p>
+					<p className="PlanView__price-gold font-size-36">$4.99</p>
+					<p className="PlanView__price-gold sub-text font-size-12">{t('per_month')}</p>
 				</Fragment>
 			</div>
-			{/* {plusButtonTop()} */}
 			<p className="card-sub-header"><strong>{t('hub_upgrade_additional_protection')}</strong></p>
-			<p className="card-sub-copy">
-				<span className="check blue" />
-				{t('hub_upgrade_basic_browser_protection')}
-			</p>
-			<p className="card-sub-copy">
-				<span className="check blue" />
-				{t('hub_upgrade_advanced_device_protection')}
-			</p>
+			<div className="PlanView__valuePropList">
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_private_search')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_tracker_protection')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_speedy_page_loads')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_intelligence_technology')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_ad_free')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_supports_ghosterys_mission')}
+				</div>
+			</div>
 		</div>
 	</div>
 );
 
-const premiumCard = mobileView => (
-	<div className="card-outer card-outer-remove">
-		<div className="card premium" data-equalizer-watch>
+const premiumCard = () => (
+	<div className="PlanView__cardOuter">
+		<div className="PlanView__card premium" data-equalizer-watch>
 			<div className="ghostery-premium-image-container">
 				<div className="ghostery-premium-image card-image-top" title="Ghostery Premium" alt="Ghostery Premium" />
 			</div>
 			<div className="ghostery-premium-image-background" />
 			<h2>Ghostery Premium</h2>
-			<div className="price">
+			<div className="PlanView__price">
 				<Fragment>
-					<p className="price-purple sub-text font-size-36">$11.99</p>
-					<p className="price-purple sub-text font-size-12">{t('per_month')}</p>
+					<p className="PlanView__price-purple sub-text font-size-36">$11.99</p>
+					<p className="PlanView__price-purple sub-text font-size-12">{t('per_month')}</p>
 				</Fragment>
 			</div>
-			{/* {premiumButtonTop()} */}
-			<p className="card-sub-header">
-				<strong>{t('hub_upgrade_maximum_browser_protection')}</strong>
-			</p>
-			<p className="card-sub-copy">
-				<span className="check blue" />
-				{t('hub_upgrade_basic_browser_protection')}
-			</p>
-			<p className="card-sub-copy">
-				<span className="check blue" />
-				{t('hub_upgrade_advanced_device_protection')}
-			</p>
-			<p className="card-sub-copy">
-				<span className="check blue" />
-				VPN
-			</p>
+			<p className="card-sub-header"><strong>{t('hub_upgrade_maximum_protection')}</strong></p>
+			<div className="PlanView__valuePropList">
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_private_search')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_tracker_protection')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_speedy_page_loads')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_intelligence_technology')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_ad_free')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_supports_ghosterys_mission')}
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					VPN
+				</div>
+				<div className="PlanView__cardSubCopy">
+					<span className="check blue" />
+					{t('hub_plan_unlimited_bandwidth')}
+				</div>
+			</div>
 		</div>
 	</div>
 );
@@ -119,17 +162,18 @@ const PlanView = () => {
 	};
 	return (
 		<div>
-			<div className="PlanView__yourPrivacyPlan">{ t('hub_plan_view_your_privacy_plan') }</div>
-			<div className="PlanView__subtitle">{ t('hub_plan_view_based_on_your_privacy_preferences') }</div>
+			<div className="PlanView__yourPrivacyPlan">{ t('hub_plan_your_privacy_plan') }</div>
+			<div className="PlanView__subtitle">{ t('hub_plan_based_on_your_privacy_preferences') }</div>
 			{searchPromo()}
-			<div className="PlanView__searchCTAButton">Start Trial</div>
-			<div className="PlanView__seeAllPlans" onClick={scrollToPlans}>{t('hub_plan_view_see_all_plans')}</div>
+			<div className="PlanView__searchCTAButton">{t('hub_plan_start_trial')}</div>
+			<div className="PlanView__seeAllPlans" onClick={scrollToPlans}>{t('hub_plan_see_all_plans')}</div>
 			<div className="PlanView__arrowDown" onClick={scrollToPlans} />
 			<div className="PlanView__plansContainer" ref={plansRef}>
 				{basicCard()}
 				{plusCard()}
 				{premiumCard()}
 			</div>
+			<div className="PlanView__searchCTAButton">{t('hub_plan_start_trial')}</div>
 		</div>
 	);
 };
