b0347c8cedc34e6c8cee1874a86636a87786dfcc
Ben <benjamin.strumeyer@gmail.com>
2020-12-02 17:53:19 -0500

    Copy jsx over from WelcomeView

---
+++ app/hub/Views/BrowserSuccessView/BrowserSuccessView.jsx
@@ -0,0 +1,30 @@
+/**
+ * Browser Success View
+ *
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2019 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+import React from 'react';
+
+/**
+ * A Functional React component for rendering the Browser Success View
+ * @return {JSX} JSX for rendering the Browser Success View of the Hub app
+ * @memberof HubComponents
+ */
+const BrowserSuccessView = () => (
+	<div className="BrowserSuccessView__container">
+		<div className="BrowserSuccessView__title">{t('hub_browser_welcome')}</div>
+		<div className="BrowserSuccessView__subtitle">{t('hub_browser_begin')}</div>
+		<img className="BrowserSuccessView__rocketShip" src="/app/images/hub/welcome/rocketShip.png" />
+		<button className="BrowserSuccessView__ctaButton" type="button">{t('hub_browser_lets_do_this')}</button>
+	</div>
+);
+
+export default BrowserSuccessView;
