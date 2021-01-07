bcf1a34527bac1c8bbb23497fd5609f092049c65
Ilya Zarembsky <wlycdgr@protonmail.com>
2021-01-07 10:36:04 -0500

    There was a coup attempt at The Capitol yesterday

--- app/ghostery-browser-hub/Views/OnboardingViews/Step3_ChooseDefaultSearchView/ChooseDefaultSearchView.jsx
+++ app/ghostery-browser-hub/Views/OnboardingViews/Step3_ChooseDefaultSearchView/ChooseDefaultSearchView.jsx
@@ -91,22 +91,24 @@ class ChooseDefaultSearchView extends Component {
 
 		return (
 			<Modal show>
-				<div className="ChooseSearchView__confirmationModalDescription">
-					Modal of type
-					{searchBeingConsidered}
+				<div className="ChooseSearchViewModal__content">
+					<div className="ChooseSearchView__confirmationModalDescription">
+						Modal of type
+						{searchBeingConsidered}
+					</div>
+					<button
+						onClick={this.cancelSelection}
+						type="button"
+					>
+						Cancel
+					</button>
+					<button
+						onClick={this.updateSelection}
+						type="button"
+					>
+						Confirm
+					</button>
 				</div>
-				<button
-					onClick={this.cancelSelection}
-					type="button"
-				>
-					Cancel
-				</button>
-				<button
-					onClick={this.updateSelection}
-					type="button"
-				>
-					Confirm
-				</button>
 			</Modal>
 		);
 	}
@@ -139,9 +141,12 @@ class ChooseDefaultSearchView extends Component {
 	render() {
 		const { modalActive } = this.state;
 
-		if (modalActive) return (this.renderConfirmationModal());
-
-		return (this.renderSearchOptions());
+		return (
+			<div className="full-height">
+				{modalActive && this.renderConfirmationModal()}
+				{!modalActive && this.renderSearchOptions()}
+			</div>
+		);
 	}
 }
 
