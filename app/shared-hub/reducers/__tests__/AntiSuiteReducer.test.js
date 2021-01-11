a870aef051159f03f8da2d74add730445618adcf
Ben <benjamin.strumeyer@gmail.com>
2021-01-11 02:02:46 -0500

    Add tests for hub reducers

---
+++ app/shared-hub/reducers/__tests__/AntiSuiteReducer.test.js
@@ -0,0 +1,69 @@
+/**
+ * AntiSuite Test Reducer
+ *
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2021 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+import Immutable from 'seamless-immutable';
+import AntiSuiteReducer from '../AntiSuiteReducer';
+import { SET_AD_BLOCK, SET_ANTI_TRACKING, SET_SMART_BLOCK } from '../../constants/AntiSuiteConstants';
+
+const initialState = Immutable({
+	setup: {
+		enable_ad_block: true,
+		enable_anti_tracking: true,
+		enable_smart_block: true
+	}
+});
+
+describe('app/shared-hub/reducers/AntiSuiteReducer', () => {
+	test('initial state is correct', () => {
+		expect(AntiSuiteReducer(undefined, {})).toEqual({});
+	});
+
+	test('reducer correctly handles SET_AD_BLOCK', () => {
+		const data = {
+			enable_ad_block: true,
+		};
+		const action = { data, type: SET_AD_BLOCK };
+
+		const updatedAntiSuiteState = Immutable.merge(initialState.setup, data);
+
+		expect(AntiSuiteReducer(initialState, action)).toEqual({
+			setup: updatedAntiSuiteState
+		});
+	});
+
+	test('reducer correctly handles SET_ANTI_TRACKING', () => {
+		const data = {
+			enable_anti_tracking: true,
+		};
+		const action = { data, type: SET_ANTI_TRACKING };
+
+		const updatedAntiSuiteState = Immutable.merge(initialState.setup, data);
+
+		expect(AntiSuiteReducer(initialState, action)).toEqual({
+			setup: updatedAntiSuiteState
+		});
+	});
+
+	test('reducer correctly handles SET_SMART_BLOCK', () => {
+		const data = {
+			enable_smart_block: true,
+		};
+		const action = { data, type: SET_SMART_BLOCK };
+
+		const updatedAntiSuiteState = Immutable.merge(initialState.setup, data);
+
+		expect(AntiSuiteReducer(initialState, action)).toEqual({
+			setup: updatedAntiSuiteState
+		});
+	});
+});
---
+++ app/shared-hub/reducers/__tests__/ToastReducer.test.js
@@ -0,0 +1,41 @@
+/**
+ * Toast Test Reducer
+ *
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2021 Ghostery, Inc. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+import Immutable from 'seamless-immutable';
+import ToastReducer from '../ToastReducer';
+import SET_TOAST from '../../constants/ToastConstants';
+
+const initialState = Immutable({
+	app: {}
+});
+
+describe('app/shared-hub/reducers/ToastReducer', () => {
+	test('initial state is correct', () => {
+		expect(ToastReducer(undefined, {})).toEqual({});
+	});
+
+	test('reducer correctly handles SET_TOAST', () => {
+		const data = {
+			toastMessage: 'hey',
+			toastClass: 'danger'
+		};
+		const action = { data, type: SET_TOAST };
+
+		const updatedToastReducerState = Immutable.merge(initialState.app, data);
+		console.log(ToastReducer(initialState, action));
+
+		expect(ToastReducer(initialState, action)).toEqual({
+			app: updatedToastReducerState
+		});
+	});
+});
