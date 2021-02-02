778601f91c36851534ad80dce31a04f699acc6cf
Ben <benjamin.strumeyer@gmail.com>
2021-02-02 14:50:39 -0500

    Fix setup_step numbers

--- app/dawn-hub/Views/OnboardingViews/Step0_WelcomeView/WelcomeView.jsx
+++ app/dawn-hub/Views/OnboardingViews/Step0_WelcomeView/WelcomeView.jsx
@@ -29,7 +29,7 @@ const WelcomeView = (props) => {
 			<div className="WelcomeView__title">{t('ghostery_dawn_onboarding_welcome')}</div>
 			<div className="WelcomeView__subtitle">{t('ghostery_dawn_onboarding_welcome_message')}</div>
 			<img className="WelcomeView__rocketShip" src="/app/images/hub/welcome/rocketShip.png" />
-			<NavLink className="WelcomeView__ctaButton" to="/onboarding/1" onClick={() => setSetupStep({ setup_step: LOGIN, origin: WELCOME })}>
+			<NavLink className="WelcomeView__ctaButton" to="/onboarding/1" onClick={() => setSetupStep({ setup_step: WELCOME, origin: WELCOME })}>
 				<span>{t('ghostery_dawn_onboarding_lets_do_this')}</span>
 			</NavLink>
 		</div>
