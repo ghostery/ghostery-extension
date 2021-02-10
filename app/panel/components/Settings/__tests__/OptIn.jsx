88fa662b0809defe534fd2dc3f1b0a8c3ff8c71e
wlycdgr <ilya.zarembsky@gmail.com>
2021-02-10 17:25:29 -0500

    Fix proptype issue with OptIn component tests

--- app/panel/components/Settings/__tests__/OptIn.jsx
+++ app/panel/components/Settings/__tests__/OptIn.jsx
@@ -22,6 +22,7 @@ describe('app/panel/Settings/OptIn.jsx', () => {
 			const settingsData = {
 				enable_metrics: false,
 				enable_human_web: false,
+				enable_abtests: false,
 			};
 
 			const component = renderer.create(
@@ -37,6 +38,7 @@ describe('app/panel/Settings/OptIn.jsx', () => {
 			const settingsData = {
 				enable_metrics: true,
 				enable_human_web: true,
+				enable_abtests: true,
 			};
 
 			const component = renderer.create(
@@ -54,6 +56,7 @@ describe('app/panel/Settings/OptIn.jsx', () => {
 			const settingsData = {
 				enable_metrics: true,
 				enable_human_web: true,
+				enable_abtests: true,
 			};
 			const toggleCheckbox = jest.fn();
 
