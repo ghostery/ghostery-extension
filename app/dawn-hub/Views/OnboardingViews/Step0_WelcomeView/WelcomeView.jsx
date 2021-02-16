2c75a312b6f7c86539689bfec3927b3695aef55d
leuryr <leuryr@yahoo.com>
2021-02-16 11:51:00 -0500

    Fix login not propagating to Dawn onboarding

--- app/dawn-hub/Views/OnboardingViews/Step0_WelcomeView/WelcomeView.jsx
+++ app/dawn-hub/Views/OnboardingViews/Step0_WelcomeView/WelcomeView.jsx
@@ -11,7 +11,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { WELCOME } from '../../OnboardingView/OnboardingConstants';
@@ -24,12 +24,32 @@ import { WELCOME } from '../../OnboardingView/OnboardingConstants';
 const WelcomeView = (props) => {
 	const { actions } = props;
 	const { setSetupStep } = actions;
+
+	const [getUserResolved, setGetUserResolved] = useState(false);
+
+	const gateSetupStep = (e) => {
+		if (getUserResolved) {
+			setSetupStep({ setup_step: WELCOME, origin: WELCOME });
+		} else {
+			e.preventDefault();
+		}
+	};
+
+	useEffect(() => {
+		actions.getUser();
+		const timer = setTimeout(() => setGetUserResolved(true), 500);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, []);
+
 	return (
 		<div className="WelcomeView__container">
 			<div className="WelcomeView__title">{t('ghostery_dawn_onboarding_welcome')}</div>
 			<div className="WelcomeView__subtitle">{t('ghostery_dawn_onboarding_welcome_message')}</div>
 			<img className="WelcomeView__rocketShip" src="/app/images/hub/welcome/rocketShip.png" />
-			<NavLink className="WelcomeView__ctaButton" to="/onboarding/1" onClick={() => setSetupStep({ setup_step: WELCOME, origin: WELCOME })}>
+			<NavLink className="WelcomeView__ctaButton" to={getUserResolved ? '/onboarding/1' : '#'} onClick={gateSetupStep}>
 				<span>{t('ghostery_dawn_onboarding_lets_do_this')}</span>
 			</NavLink>
 		</div>
