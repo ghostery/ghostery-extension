54c2138b70fe846f256a993e71726a309436abdf
Krzysztof Jan Modras <chrmod@chrmod.net>
2021-10-30 17:29:07 +0200

    Report broken page: simplify email template

--- extension-manifest-v3/src/panel/components/panel-footer.js
+++ extension-manifest-v3/src/panel/components/panel-footer.js
@@ -9,7 +9,7 @@ define({
   tag: "panel-footer",
   content: () => html`
     <footer>
-      <a href="mailto:support@ghostery.com?body=PLEASE%20INCLUDE%20A%20DESCRIPTION%20AND%20A%20PICTURE%20OF%20THE%20ISSUE%20YOU%20ARE%20EXPERIENCING%3A%0D%0A%0D%0A%0D%0A%0D%0A%0D%0A%0D%0AURL%3A%20https%3A%2F%2Fwww.ghostery.com%2Fdata-request%0D%0AGhostery%20version%3A%208.5.9%0D%0ADatabase%20Version%3A%20d1768a1010766a98e79d9af3e925e260%0D%0ABrowser%20name%3A%20Ghostery%20Desktop%20Browser%0D%0ABrowser%20version%3A%2020219%0D%0ALanguage%3A%20en%0D%0AOS%3A%20mac%0D%0A%0D%0ACategory%3A%20site_analytics%0D%0AAllowed%20Trackers%3A%20Matomo%0D%0ABlocked%20Trackers%3A%20%0D%0A&subject=Broken Page Report">
+      <a href="mailto:support@ghostery.com?body=PLEASE%20INCLUDE%20A%20DESCRIPTION%20AND%20A%20PICTURE%20OF%20THE%20ISSUE%20YOU%20ARE%20EXPERIENCING&subject=Broken Page Report">
         ${t('panel_menu_report_broken_site')}
       </a>
       <span class="dot">&middot;</span>
