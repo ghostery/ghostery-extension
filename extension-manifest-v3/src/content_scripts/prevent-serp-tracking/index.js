39520c7c30f373a30fc4e3f9c2aab8ab18c52444
Philipp Claßen <philipp@cliqz.com>
2021-11-17 21:16:09 +0100

    Prevent various methods from Google for tracking user interactions (#45)
    Co-authored-by: Krzysztof Jan Modras <chrmod@chrmod.net>

---
+++ extension-manifest-v3/src/content_scripts/prevent-serp-tracking/index.js
@@ -0,0 +1,60 @@
+/**
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2017-present Ghostery GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+function findTopLink(elem) {
+  while (elem && !elem.href) {
+    elem = elem.parentElement;
+  }
+  return elem;
+}
+
+function safeLinkClick(event) {
+  const link = findTopLink(event.target);
+  if (!link) {
+    return;
+  }
+
+  link.removeAttribute('ping');
+  link.removeAttribute('onmousedown');
+  event.stopImmediatePropagation();
+
+  try {
+    const directUrl = new URL(link.href).searchParams.get('url');
+    if (directUrl) {
+      console.debug('safeLinkClick changed:', link.href, '->', directUrl);
+      window.location = directUrl;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function linkCleaner(event) {
+    const link = findTopLink(event.target);
+    if (!link) {
+      return;
+    }
+
+    link.removeAttribute('ping');
+    try {
+      const directUrl = new URL(link.href).searchParams.get('url');
+      if (directUrl) {
+        console.debug('linkCleaner changed:', link.href, '->', directUrl);
+        link.href = directUrl;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+}
+
+document.addEventListener('click', safeLinkClick, true);
+document.addEventListener('onmousedown', linkCleaner, true);
+document.addEventListener('touchstart', linkCleaner, true);
