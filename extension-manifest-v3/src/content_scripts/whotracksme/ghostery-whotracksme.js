aabce58e4138d866e5df46fe0bbde005b0c114e6
Krzysztof Modras <chrmod@chrmod.net>
2021-11-18 10:40:05 +0100

    WTM: detect more trackers  (#48)
    * WTM: detect trackers triggered via fetch, XMLHTTPRequest, Image & sendBeacon
    
    * fixes

---
+++ extension-manifest-v3/src/content_scripts/whotracksme/ghostery-whotracksme.js
@@ -0,0 +1,84 @@
+/**
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ * https://www.whotracks.me/
+ *
+ * Copyright 2017-present Ghostery GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+(function () {
+  function sendMessage(url) {
+    if (!url || url.startsWith('data:')) {
+      return;
+    }
+    window.postMessage(`GhosteryTrackingDetection:${encodeURIComponent(url)}`, '*');
+  }
+
+  let originalXHROpen = null;
+  let originalXHRSend = null;
+  let originalFetch = null;
+  let originalImageSrc = null;
+  let originalSendBeacon = null;
+
+  // -------------------------------------------------
+  // XMLHttpRequest
+  // -------------------------------------------------
+  originalXHROpen = XMLHttpRequest.prototype.open;
+  originalXHRSend = XMLHttpRequest.prototype.send;
+
+  const _url = new WeakMap();
+
+  XMLHttpRequest.prototype.open = function(method, url) {
+    _url.set(this, url);
+    return originalXHROpen.apply(this, arguments);
+  };
+
+  XMLHttpRequest.prototype.send = function(body) {
+    sendMessage(_url.get(this));
+    return originalXHRSend.apply(this, arguments);
+  };
+
+  // -------------------------------------------------
+  // fetch
+  // -------------------------------------------------
+  originalFetch = window.fetch;
+
+  window.fetch = function(input, init) {
+    if (typeof input === 'string') {
+      sendMessage(input);
+    } else if (input instanceof Request) {
+      sendMessage(input.url);
+    }
+
+    var result = originalFetch.apply(window, arguments);
+    return result;
+  };
+
+  // -------------------------------------------------
+  // Image
+  // -------------------------------------------------
+  originalImageSrc = Object.getOwnPropertyDescriptor(Image.prototype, 'src');
+
+  delete Image.prototype.src;
+  Object.defineProperty(Image.prototype, 'src', {
+    get: function() {
+      return originalImageSrc.get.call(this);
+    },
+    set: function(value) {
+      sendMessage(value);
+      originalImageSrc.set.call(this, value);
+    }
+  });
+
+  // -------------------------------------------------
+  // sendBeacon
+  // -------------------------------------------------
+  originalSendBeacon = window.Navigator.prototype.sendBeacon;
+  window.Navigator.prototype.sendBeacon = function(url, data) {
+    sendMessage(url);
+    return originalSendBeacon.apply(window.navigator, arguments);
+  };
+})();
---
+++ extension-manifest-v3/src/content_scripts/whotracksme/index.js
@@ -0,0 +1,124 @@
+/**
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ * https://www.whotracks.me/
+ *
+ * Copyright 2017-present Ghostery GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+function postMessage({ urls }) {
+  chrome.runtime.sendMessage({ action: "updateTabStats", args: [{ urls }]});
+}
+
+const origin = (new URL(window.location.href)).origin;
+
+const start = Date.now();
+let loadTime = 0;
+
+window.addEventListener('load', () => {
+  loadTime = Date.now() - start;
+  chrome.runtime.sendMessage({ action: "updateTabStats", args: [{ loadTime }]});
+});
+
+// Based on https://github.com/mozilla-mobile/firefox-ios/blob/1f3fd1640214b2b442c573ea7d2882d480f4f24c/content-blocker-lib-ios/js/TrackingProtectionStats.js
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+(function install() {
+  let sendUrls = new Array();
+  let sendUrlsTimeout = null;
+
+  function sendMessage(url, source) {
+    if (!url || url.startsWith('data:')) {
+      return;
+    }
+
+    if (url) {
+      sendUrls.push(url)
+    }
+
+    // If already set, return
+    if (sendUrlsTimeout) return;
+
+    // Send the URLs in batches every 200ms to avoid perf issues
+    // from calling js-to-native too frequently.
+    sendUrlsTimeout = setTimeout(() => {
+      sendUrlsTimeout = null;
+      if (sendUrls.length < 1) return;
+      postMessage({ urls: sendUrls });
+      sendUrls = new Array();
+    }, 200);
+  }
+
+  function onLoadNativeCallback() {
+    // Send back the sources of every script and image in the DOM back to the host application.
+    [].slice.apply(document.scripts).forEach(function(el) { sendMessage(el.src, 'load script'); });
+    [].slice.apply(document.images).forEach(function(el) { sendMessage(el.src, 'load image'); });
+    [].slice.apply(document.getElementsByTagName('iframe')).forEach(function(el) { sendMessage(el.src, 'load iframe'); })
+  }
+
+  window.addEventListener("load", onLoadNativeCallback, false);
+
+  const mutationObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      mutation.addedNodes.forEach(function(node) {
+        // `<script src="*">` elements.
+        if (node.tagName === "SCRIPT" && node.src) {
+          sendMessage(node.src, 'mutation script');
+          return;
+        }
+        if (node.tagName === "IMG" && node.src) {
+          sendMessage(node.src, 'mutation image');
+          return;
+        }
+
+        // `<iframe src="*">` elements where [src] is not "about:blank".
+        if (node.tagName === "IFRAME" && node.src) {
+          if (node.src === "about:blank") {
+            return;
+          }
+
+          sendMessage(node.src, 'mutation iframe');
+          return;
+        }
+
+        // `<link href="*">` elements.
+        if (node.tagName === "LINK" && node.href) {
+          sendMessage(node.href, 'mutation link');
+        }
+      });
+    });
+  });
+
+  mutationObserver.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  });
+
+  // fetch, XMLHTTPRequest and others must be injected in main world
+  function injectMonkeyPatches() {
+    const script = document.createElement('script');
+    script.src = chrome.runtime.getURL("content_scripts/whotracksme/ghostery-whotracksme.js");
+    script.onload = function() {
+      this.remove();
+    };
+    (document.head || document.documentElement).appendChild(script);
+
+    window.addEventListener('message', (message) => {
+      if (!message.data.startsWith('GhosteryTrackingDetection:')) {
+        return;
+      }
+      let url = decodeURIComponent(message.data.split(':')[1]);
+      if (url.startsWith('/')) {
+        url = `${origin}${url}`;
+      }
+      sendMessage(url, 'content');
+    });
+  }
+
+  injectMonkeyPatches();
+})();
