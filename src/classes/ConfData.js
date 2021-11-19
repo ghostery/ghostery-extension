548397f65cb6fa7637455216e7d45eac15bfa7b2
Philipp Claßen <philipp@cliqz.com>
2021-11-19 11:40:13 +0100

    Make the Tracker-Tally opt-in for new installations.

--- src/classes/ConfData.js
+++ src/classes/ConfData.js
@@ -135,7 +135,7 @@ class ConfData {
 			_initProperty('paid_subscription', false);
 			_initProperty('settings_last_imported', 0);
 			_initProperty('settings_last_exported', 0);
-			_initProperty('show_alert', !IS_ANDROID);
+			_initProperty('show_alert', false); // Tracker-Tally
 			_initProperty('show_badge', true);
 			_initProperty('show_cmp', true);
 			_initProperty('show_tracker_urls', true);
