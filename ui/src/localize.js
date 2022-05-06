d10d7196158913926af3bab1821e6803fc2c6f43
Dominik Lubański <dominik.lubanski@gmail.com>
2022-05-06 15:56:13 +0200

    Use chrome.18n format option

--- ui/src/localize.js
+++ ui/src/localize.js
@@ -1,11 +1,5 @@
 import { localize } from 'hybrids';
 
 if (typeof chrome === 'object' && chrome.i18n) {
-  localize((key) => {
-    const translation = chrome.i18n.getMessage(key);
-    if (!translation) {
-      console.warn(`Ghostery: missing translation for key: ${key}`);
-    }
-    return translation || key;
-  });
+  localize(chrome.i18n.getMessage, { format: 'chrome.i18n' });
 }
