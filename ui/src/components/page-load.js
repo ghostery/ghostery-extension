4648c69327da49b3d00a8f2089a29d22f5e3ec8d
Dominik Lubański <dominik.lubanski@gmail.com>
2022-05-06 15:53:10 +0200

    Add test and release actions (#1)

--- ui/src/components/page-load.js
+++ ui/src/components/page-load.js
@@ -9,46 +9,46 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
 
- import { html, define } from 'hybrids';
+import { html, define } from 'hybrids';
 
- export default define({
-   tag: 'ui-page-load',
-   loadTime: {
-     get: (_, val = 0) => val,
-     set: (_, val) => Math.round(val),
-   },
-   color: ({ loadTime }) => {
-     if (loadTime < 100) {
-       return '#779D3E';
-     } else if (loadTime < 500) {
-       return '#BB9556';
-     } else {
-       return '#8D4144';
-     }
-   },
-   render: ({ loadTime, color }) => html`
-     <div class="info">Page Load:</div>
-     <div class="circle">
-       <svg
-         width="73"
-         height="72"
-         viewBox="0 0 73 72"
-         fill="none"
-         xmlns="http://www.w3.org/2000/svg"
-       >
-         <circle
-           cx="36.5003"
-           cy="36.2249"
-           r="30.7273"
-           stroke="currentColor"
-           stroke-width="10"
-           stroke-dasharray="1 3"
-         />
-       </svg>
-       <strong>${loadTime}</strong>
-     </div>
-     <div></div>
-   `.css`
+export default define({
+  tag: 'ui-page-load',
+  loadTime: {
+    get: (_, val = 0) => val,
+    set: (_, val) => Math.round(val),
+  },
+  color: ({ loadTime }) => {
+    if (loadTime < 100) {
+      return '#779D3E';
+    } else if (loadTime < 500) {
+      return '#BB9556';
+    } else {
+      return '#8D4144';
+    }
+  },
+  render: ({ loadTime, color }) => html`
+    <div class="info">Page Load:</div>
+    <div class="circle">
+      <svg
+        width="73"
+        height="72"
+        viewBox="0 0 73 72"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="36.5003"
+          cy="36.2249"
+          r="30.7273"
+          stroke="currentColor"
+          stroke-width="10"
+          stroke-dasharray="1 3"
+        />
+      </svg>
+      <strong>${loadTime}</strong>
+    </div>
+    <div></div>
+  `.css`
      :host {
        display: grid;
        grid-template-columns: 1fr 1fr 1fr;
@@ -97,5 +97,4 @@
        text-align: center;
      }
    `,
- });
- 
+});
