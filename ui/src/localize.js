b2bb3893998d27f505cd2b96eabf0cb0a86df0b2
Dominik Lubański <dominik.lubanski@gmail.com>
2022-05-17 15:06:08 +0200

    Fix getMessage function to work in Safari (#6)

--- ui/src/localize.js
+++ ui/src/localize.js
@@ -1,5 +1,5 @@
 import { localize } from 'hybrids';
 
 if (typeof chrome === 'object' && chrome.i18n) {
-  localize(chrome.i18n.getMessage, { format: 'chrome.i18n' });
+  localize(chrome.i18n.getMessage.bind(chrome.i18n), { format: 'chrome.i18n' });
 }
