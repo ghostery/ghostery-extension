67a1cfa71a9a3be8a30329ba04ec76d6d6a26421
Krzysztof Modras <chrmod@chrmod.net>
2022-09-23 17:34:34 +0200

    Android: ensure anolysis is not enabled

--- extension-manifest-v2/src/classes/Common.js
+++ extension-manifest-v2/src/classes/Common.js
@@ -36,7 +36,7 @@ COMMON.config.default_prefs = {
 	'modules.hpnv2.enabled': false,
 	'modules.human-web-lite.enabled': false,
 	'modules.hpn-lite.enabled': false,
-	'modules.anolysis.enabled': IS_ANDROID,
+	'modules.anolysis.enabled': !IS_ANDROID,
 	'modules.insights.enabled': true,
 };
 
@@ -71,6 +71,8 @@ common.start = async () => {
 	WTM.CHANNEL = HW_CHANNEL;
 	COMMON.config.settings.HW_CHANNEL = HW_CHANNEL;
 
+	setPref('modules.anolysis.enabled', !IS_ANDROID);
+	
 	if (!IS_ANDROID) {
 		setPref('modules.human-web.enabled', conf.enable_human_web);
 		setPref('modules.hpnv2.enabled', conf.enable_human_web);
