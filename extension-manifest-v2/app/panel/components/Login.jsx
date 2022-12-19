521b50533debad56b84f161f615ee6ae81dde0d3
Krzysztof Modras <chrmod@chrmod.net>
2022-12-19 15:18:06 +0100

    Remove dead dependencies

--- extension-manifest-v2/app/panel/components/Login.jsx
+++ extension-manifest-v2/app/panel/components/Login.jsx
@@ -14,7 +14,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ClassNames from 'classnames';
-import RSVP from 'rsvp';
 import globals from '../../../src/classes/Globals';
 import { validateEmail } from '../utils/utils';
 import { log } from '../../../src/utils/common';
@@ -66,7 +65,7 @@ class Login extends React.Component {
 			actions.login(email, password)
 				.then((success) => {
 					if (success) {
-						RSVP.all([
+						Promise.all([
 							actions.getUser(),
 							actions.getUserSettings(),
 						])
