cb80ff8f0229328bc99e2d53c799fd45f549e812
Dominik Lubański <dominik.lubanski@gmail.com>
2023-01-16 12:50:22 +0100

    Move license generation to build script & minor fixes

--- tools/licenses.js
+++ tools/licenses.js
@@ -15,15 +15,15 @@ const checker = require('license-checker');
 const fs = require('fs');
 
 const IGNORED_PACAKGES = [
-	'@ghostery/libs',
-	'@ghostery/ui',
-	'ghostery-common',
-	'@ghostery/extension-manifest-v2',
-	'@ghostery/extension-manifest-v3',
-	'@types/',
+  '@ghostery/libs',
+  '@ghostery/ui',
+  'ghostery-common',
+  '@ghostery/extension-manifest-v2',
+  '@ghostery/extension-manifest-v3',
+  '@types/',
 ];
 
-const template= (packages) => `
+const template = (packages) => `
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -46,59 +46,81 @@ const template= (packages) => `
 	</head>
 	<body>
 		<h1>Licenses</h1>
-		${packages.map((package) => `
+		${packages
+      .map(
+        (package) => `
 			<h2>${package.name}</h2>
 			<ul>
-				${package.publisher ? `<li><label>Publisher: </label>${package.publisher}</li>` : ''}
+				${
+          package.publisher
+            ? `<li><label>Publisher: </label>${package.publisher}</li>`
+            : ''
+        }
 				${package.email ? `<li><label>Email: </label>${package.email}</li>` : ''}
-				${package.repository ? `<li><label>Repository: </label>${package.repository}</li>` : ''}
+				${
+          package.repository
+            ? `<li><label>Repository: </label>${package.repository}</li>`
+            : ''
+        }
 				${package.url ? `<li><label>URL: </label>${package.url}</li>` : ''}
-				${package.licenses ? `<li><label>License: </label>${package.licenses}</li>` : ''}
+				${
+          package.licenses
+            ? `<li><label>License: </label>${package.licenses}</li>`
+            : ''
+        }
 			</ul>
 			${package.licenseText ? `<pre>${package.licenseText}</pre>` : ''}
-		`).join('')}
+		`,
+      )
+      .join('')}
 	</body>
 </html>
 `;
 
 // Build list of licenses
-checker.init({
-	start: '.',
-	production: true,
-	json: true,
-	direct: false,
-}, (err, licenseJSON) => {
-	if (err) {
-		console.error('License Fetcher error:', err);
-		return;
-	}
+checker.init(
+  {
+    start: '.',
+    production: true,
+    json: true,
+    direct: false,
+  },
+  (err, licenseJSON) => {
+    if (err) {
+      console.error('License Fetcher error:', err);
+      return;
+    }
 
-	console.log('Generating licenses.html');
-	const output = {};
+    console.log('Generating licenses.html');
+    const output = {};
 
-	Object.keys(licenseJSON).forEach((packageName) => {
-		if (IGNORED_PACAKGES.some(name => packageName.startsWith(name))) {
-			return;
-		}
+    Object.keys(licenseJSON).forEach((packageName) => {
+      if (IGNORED_PACAKGES.some((name) => packageName.startsWith(name))) {
+        return;
+      }
 
-		output[packageName] = {
-			name: packageName,
-			repository: licenseJSON[packageName].repository,
-			licenses: licenseJSON[packageName].licenses,
-			publisher: licenseJSON[packageName].publisher,
-			url: licenseJSON[packageName].url,
-			email: licenseJSON[packageName].email,
-		};
+      output[packageName] = {
+        name: packageName,
+        repository: licenseJSON[packageName].repository,
+        licenses: licenseJSON[packageName].licenses,
+        publisher: licenseJSON[packageName].publisher,
+        url: licenseJSON[packageName].url,
+        email: licenseJSON[packageName].email,
+      };
 
-		if (licenseJSON[packageName].licenseFile) {
-			output[packageName].licenseText = fs.readFileSync(licenseJSON[packageName].licenseFile, {encoding:'utf8', flag:'r'});
-		}
-	});
+      if (licenseJSON[packageName].licenseFile) {
+        output[packageName].licenseText = fs.readFileSync(
+          licenseJSON[packageName].licenseFile,
+          { encoding: 'utf8', flag: 'r' },
+        );
+      }
+    });
 
-	if (!fs.existsSync('dist')) {
-		fs.mkdirSync('dist');
-	}
+    if (!fs.existsSync('dist')) {
+      fs.mkdirSync('dist');
+    }
 
-	fs.writeFileSync('./dist/licenses.html', template(Object.values(output)));
-	console.log('Completed licenses.html');
-});
+    fs.writeFileSync('./dist/licenses.html', template(Object.values(output)));
+    console.log('Completed licenses.html');
+  },
+);
