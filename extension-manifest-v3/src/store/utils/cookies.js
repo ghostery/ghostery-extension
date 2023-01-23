06df0fa79c8a6de0175dac4927dd2edbc7f3e4e6
Dominik Lubański <dominik.lubanski@gmail.com>
2023-01-23 13:39:47 +0100

    Add license notice

--- extension-manifest-v3/src/store/utils/cookies.js
+++ extension-manifest-v3/src/store/utils/cookies.js
@@ -1,3 +1,14 @@
+/**
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2017-present Ghostery GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
 const COOKIE_DOMAIN = '.ghosterystage.com';
 const COOKIE_URL = 'https://ghosterystage.com';
 const COOKIE_DURATION = 60 * 60 * 24 * 90; // 90 days in seconds
