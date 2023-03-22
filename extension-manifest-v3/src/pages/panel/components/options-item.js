fce36aee5f949bc050c69e87a938b1525e366275
Dominik Lubański <dominik.lubanski@gmail.com>
2023-03-22 10:46:16 +0100

    Add gap to split option name and value

--- extension-manifest-v3/src/pages/panel/components/options-item.js
+++ extension-manifest-v3/src/pages/panel/components/options-item.js
@@ -16,7 +16,7 @@ export default {
   enabled: false,
   terms: false,
   render: ({ icon, enabled, terms }) => html`
-    <template layout="row items:center padding:1:1.5">
+    <template layout="row gap items:center padding:1:1.5">
       ${icon && html`<ui-icon name="${icon}" layout="margin:right"></ui-icon>`}
       <ui-text type="body-s" color="gray-900" layout="grow">
         <slot></slot>
