3d3884b9e34997c0fbbfe3cc464438ff00498c7a
Krzysztof Modras <chrmod@chrmod.net>
2023-08-09 14:37:59 +0200

    Fix safari startup without webRequest

--- extension-manifest-v3/src/background/index.js
+++ extension-manifest-v3/src/background/index.js
@@ -8,7 +8,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
-
+import './env.js';
 import './onboarding.js';
 
 import './autoconsent.js';
