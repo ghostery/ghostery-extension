a7940afc17da52737373c62d3f2a010a31b84971
Dominik Lubański <dominik.lubanski@gmail.com>
2023-08-11 12:20:20 +0200

    Sync migrated options for logged in users

--- extension-manifest-v3/src/store/options.js
+++ extension-manifest-v3/src/store/options.js
@@ -87,6 +87,8 @@ const Options = {
 
       // Migrate options
       if (optionsVersion < 1) {
+        const keys = [];
+
         // Migrate from Extension v8 (MV2)
         if (__PLATFORM__ !== 'safari') {
           options = migrateFromMV2();
@@ -100,6 +102,8 @@ const Options = {
           options.blockAds = true;
           options.blockTrackers = true;
           options.blockAnnoyances = true;
+
+          keys.push('blockAds', 'blockTrackers', 'blockAnnoyances');
         }
 
         // Flush updated options and version to the storage
@@ -107,6 +111,9 @@ const Options = {
           options,
           optionsVersion: 1,
         });
+
+        // Send updated options to the server
+        Promise.resolve().then(() => sync(options, keys));
       }
 
       return options;
