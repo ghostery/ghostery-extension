5106050e8f00c07f0617d2127bb8575833bc1a77
Dominik Lubański <dominik.lubanski@gmail.com>
2023-08-25 11:37:47 +0200

    Rename general `alarms.js` to `paused.js`

--- extension-manifest-v3/src/background/paused.js
+++ extension-manifest-v3/src/background/paused.js
@@ -15,19 +15,6 @@ import Options, { observe } from '/store/options.js';
 // Pause / unpause domains
 const PAUSED_ALARM_PREFIX = 'options:revoke';
 
-// Remove paused domains from options when alarm is triggered
-chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name.startsWith(PAUSED_ALARM_PREFIX)) {
-    store.resolve(Options).then((options) => {
-      store.set(options, {
-        paused: options.paused.filter(
-          ({ id }) => `${PAUSED_ALARM_PREFIX}:${id}` !== alarm.name,
-        ),
-      });
-    });
-  }
-});
-
 observe('paused', async (paused) => {
   const alarms = (await chrome.alarms.getAll()).filter(({ name }) =>
     name.startsWith(PAUSED_ALARM_PREFIX),
@@ -52,5 +39,18 @@ observe('paused', async (paused) => {
           when: revokeAt,
         });
       });
+  }
+});
+
+// Remove paused domains from options when alarm is triggered
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name.startsWith(PAUSED_ALARM_PREFIX)) {
+    store.resolve(Options).then((options) => {
+      store.set(options, {
+        paused: options.paused.filter(
+          ({ id }) => `${PAUSED_ALARM_PREFIX}:${id}` !== alarm.name,
+        ),
+      });
+    });
   }
 });
