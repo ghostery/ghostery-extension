4be2d3c9cc3aee2fca60e683b171c85a6afb2d07
Krzysztof Modras <chrmod@chrmod.net>
2023-09-06 17:53:12 +0200

    CI: add commit sha and upload artifacts

--- extension-manifest-v3/scripts/package.sh
+++ extension-manifest-v3/scripts/package.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+SHA=''
+for val in $@; do
+  if [ $val == '--sha' ]; then
+    SHA=$(git rev-parse --short HEAD)
+  fi
+done
+
 VERSION=$(node -p "require('./package.json').version")
 
-for PLATFORM in firefox chrome edge opera
-do
+for PLATFORM in firefox chrome edge opera; do
   npm run build -- $PLATFORM --silent
   npm exec web-ext -- build \
     --overwrite-dest \
-    -n ghostery-$PLATFORM-$VERSION.zip
+    -n ghostery-$PLATFORM-$VERSION$([ $SHA ] && echo "-$SHA").zip
 done
