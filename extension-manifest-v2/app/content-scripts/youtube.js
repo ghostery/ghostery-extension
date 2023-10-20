8bd6b965d526769ee9fee8f70738cb1d76b606df
Krzysztof Modras <chrmod@chrmod.net>
2023-10-20 20:38:33 +0200

    Deal with YT navigation

--- extension-manifest-v2/app/content-scripts/youtube.js
+++ extension-manifest-v2/app/content-scripts/youtube.js
@@ -1,7 +1,19 @@
-import { showIframe } from '@ghostery/ui/iframe';
+import { showIframe, closeIframe } from '@ghostery/ui/iframe';
+
+function show() {
+	showIframe(chrome.runtime.getURL(`app/templates/youtube.html?url=${encodeURIComponent(window.location.href)}`), '460px');
+}
 
 function start() {
-	showIframe(chrome.runtime.getURL(`app/templates/youtube.html?url=${encodeURIComponent(window.location.href)}`), '460px');
+	window.addEventListener('yt-navigate-start', () => {
+		closeIframe();
+	}, true);
+
+	window.addEventListener('yt-navigate-finish', () => {
+		show();
+	}, true);
+
+	show();
 }
 
 chrome.storage.local.get(['youtube_dont_show_again'], (storage) => {
