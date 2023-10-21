6e31b174013d3a8961e36d54c438043654213228
Krzysztof Modras <chrmod@chrmod.net>
2023-10-21 14:53:25 +0200

    Periodic detection

--- extension-manifest-v2/app/content-scripts/youtube.js
+++ extension-manifest-v2/app/content-scripts/youtube.js
@@ -3,13 +3,10 @@ import { showIframe, closeIframe } from '@ghostery/ui/iframe';
 // Based on https://github.com/AdguardTeam/AdguardFilters/blob/e5ae8e3194f8d18bdcc660d4c42282e4a96ca5b9/AnnoyancesFilter/Popups/sections/antiadblock.txt#L2044
 const ADBLOCKER_WALL_SELECTORS = [
 	'ytd-enforcement-message-view-model > div.ytd-enforcement-message-view-model',
+	'ytd-watch-flexy[player-unavailable] #player-container-outer.ytd-watch-flexy',
 ];
 
-function tryToShow() {
-	if (document.querySelectorAll(ADBLOCKER_WALL_SELECTORS).length > 0) {
-		showIframe(chrome.runtime.getURL(`app/templates/youtube.html?url=${encodeURIComponent(window.location.href)}`), '460px');
-	}
-}
+let isShown = false;
 
 chrome.storage.local.get(['youtube_dont_show_again'], (storage) => {
 	if (storage.youtube_dont_show_again || chrome.extension.inIncognitoContext) {
@@ -17,12 +14,14 @@ chrome.storage.local.get(['youtube_dont_show_again'], (storage) => {
 	}
 
 	window.addEventListener('yt-navigate-start', () => {
+		isShown = false;
 		closeIframe();
 	}, true);
 
-	window.addEventListener('yt-navigate-finish', () => {
-		tryToShow();
-	}, true);
-
-	tryToShow();
+	setInterval(() => {
+		if (!isShown && document.querySelectorAll(ADBLOCKER_WALL_SELECTORS).length > 0) {
+			showIframe(chrome.runtime.getURL(`app/templates/youtube.html?url=${encodeURIComponent(window.location.href)}`), '460px');
+			isShown = true;
+		}
+	}, 2000);
 });
