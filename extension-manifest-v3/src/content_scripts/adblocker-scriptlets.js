a6a7ad7d94414fa492a937cd21e599a659d743b9
Krzysztof Modras <chrmod@chrmod.net>
2023-11-02 12:08:06 +0100

    Rename the content-script message

--- extension-manifest-v3/src/content_scripts/adblocker-scriptlets.js
+++ extension-manifest-v3/src/content_scripts/adblocker-scriptlets.js
@@ -2,4 +2,4 @@
 // the tabId of the initial chrome.webNavigation.onCommitted
 // is not reliable. When opening bookmarks, it can happen that
 // the event is associated with a tabId of 0.
-chrome.runtime.sendMessage({ action: 'onCommitted' });
+chrome.runtime.sendMessage({ action: 'injectScriptlets' });
