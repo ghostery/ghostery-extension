d520567c44e21a58d0a09b018300b64f192bb763
Dominik Lubański <dominik.lubanski@gmail.com>
2023-11-21 09:51:16 +0100

    Clean up download trackers preview script

--- packages/trackers-preview/scripts/download.js
+++ packages/trackers-preview/scripts/download.js
@@ -1,12 +1,10 @@
 import { writeFileSync } from 'fs';
 import { URL, fileURLToPath } from 'url';
-import { resolve } from 'path';
 
 const DATA_URL = 'https://whotracks.me/data/trackers-preview.json';
-const OUTPUT_FILE = fileURLToPath(new URL(
-  '../src/background/trackers-preview-generated.js',
-  import.meta.url,
-));
+const OUTPUT_FILE = fileURLToPath(
+  new URL('../src/background/trackers-preview-generated.js', import.meta.url),
+);
 
 const data = await fetch(DATA_URL).then((res) => {
   if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
