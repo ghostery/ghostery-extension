ghostery/ghostery-extension

commit d4a106e6eb5a8c54ab25d5d719941b33d8ef578d

Fix tabUrl & clean up empty onHeadersReceived callback

extension-manifest-v2/src/background.js
@@ -1018,18 +1018,6 @@ function initializeDispatcher() {
 		common.modules.core.action('refreshAppState');
 	});
 }
-/**
- * Checks if the origin of the request comes from whitelisted sites
- * @param {webRequestPipeline.request} state
- * @returns boolean
- */
-function isOriginWhitelisted(state) {
-	if (state.originUrlParts && conf.site_whitelist.includes(state.originUrlParts.hostname)) {
-		return true;
-	}
-
-	return false;
-}
 
 /**
  * WebRequest pipeline initialization: find which Common modules are enabled,
@@ -1067,31 +1055,13 @@ function initialiseWebRequestPipeline() {
 			spec: 'blocking',
 			before: existingSteps.onBeforeRequest,
 			fn: (state, response) => {
-				if (isOriginWhitelisted(state)) {
-					return true;
-				}
-
 				const result = events.onBeforeRequest(state, response);
 				if (result.cancel || result.redirectUrl) {
 					Object.assign(response, result);
 				}
-
 				return true;
 			}
 		}),
-		webRequestPipeline.action('addPipelineStep', 'onHeadersReceived', {
-			name: 'ghostery.onHeadersReceived',
-			spec: 'collect',
-			before: existingSteps.onHeadersReceived,
-			fn: (state) => {
-				if (isOriginWhitelisted(state)) {
-					return true;
-				}
-
-				Events.onHeadersReceived(state);
-				return true;
-			}
-		})
 	]);
 }
 /**
@@ -1106,18 +1076,12 @@ function isWhitelistedForAdblocking(state) {
 	if (globals.SESSION.paused_blocking) {
 		return true;
 	}
-
-	if (isOriginWhitelisted(state)) {
-		return true;
-	}
-
 	if (state.onlyCheckPause) {
 		// This should only be reachable in the context of push injections,
 		// which lack the context. But push injection handled whitelisted
 		// pages already in a previous step.
 		return false;
 	}
-
 	return (
 		Boolean(state.ghosteryWhitelisted)
 		|| (Policy.getSitePolicy(state.tabUrl, state.url) === 2)
@@ -1129,7 +1093,6 @@ function isWhitelistedForAdblocking(state) {
 function isWhitelistedForAntiTracking(state) {
 	return (
 		Boolean(globals.SESSION.paused_blocking)
-		|| isOriginWhitelisted(state)
 		|| (Policy.getSitePolicy(state.tabUrl, state.url) === 2)
 		// only check common_checklist if the tracker id is unknown
 		|| (!state.ghosteryBug && Policy.checkCommonModuleWhitelist(state.tabUrlParts.domainInfo.domain, state.urlParts.domainInfo.domain))

extension-manifest-v2/src/classes/EventHandlers.js
@@ -286,7 +286,7 @@ class EventHandlers {
 		}
 
 		// TODO fuse this into a single call to improve performance
-		const page_url = tabInfo.getTabInfo(tab_id, 'url');
+		const page_url = eventMutable.tabUrl;
 		const {
 			patternId: bug_id,
 			isFilterMatched,
@@ -421,17 +421,6 @@ class EventHandlers {
 		}
 
 		return { requestHeaders: details.requestHeaders };
-	}
-
-	/**
-	 * Handler for webRequest.onHeadersReceived event
-	 * Called each time that an HTTP(S) response header is received.
-	 *
-	 * @param  {Object} details 	event data
-	 */
-	static onHeadersReceived(details) {
-		// Skip content-length collection if it's a 3XX (redirect)
-		if (details.statusCode >> 8 === 1) { } // eslint-disable-line no-bitwise, no-empty
 	}
 
 	/**

commit 48fc237d396317a842846a6793ec271a3fff1d84

Fixes the startup error: (#1444)
InitError: Invalid listener for webRequest.onHeadersReceived

Came with a regression in d4a106e6eb5a8c54ab25d5d719941b33d8ef578d,
where the "onHeadersReceived" handler was removed from Events and
thus became "undefined" in the import (making it an invalid event handler).

extension-manifest-v2/src/background.js
@@ -81,7 +81,6 @@ const VERSION_CHECK_URL = `${CDN_BASE_URL}/update/v4.1/versions.json`;
 const ONE_HOUR_MSEC = 3600000;
 const ONE_DAY_MSEC = ONE_HOUR_MSEC * 24;
 const onBeforeRequest = events.onBeforeRequest.bind(events);
-const { onHeadersReceived } = Events;
 
 /**
  * Pulls down latest version.json and triggers
@@ -1038,13 +1037,11 @@ function initialiseWebRequestPipeline() {
 	}
 	// remove ghostery listeners from standard webrequest events
 	chrome.webRequest.onBeforeRequest.removeListener(onBeforeRequest);
-	chrome.webRequest.onHeadersReceived.removeListener(onHeadersReceived);
 
 	// look for steps from other modules which we need to be before
-	const existingSteps = { onBeforeRequest: [], onHeadersReceived: [] };
+	const existingSteps = { onBeforeRequest: [] };
 	if (common.modules.antitracking.isEnabled) {
 		existingSteps.onBeforeRequest.push('common.modules.antitracking.onBeforeRequest');
-		existingSteps.onHeadersReceived.push('common.modules.antitracking.onHeadersReceived');
 	}
 	if (common.modules.adblocker.isEnabled) {
 		existingSteps.onBeforeRequest.push('adblocker');
@@ -1197,7 +1194,6 @@ function addCommonGhosteryAndAntitrackingListeners() {
 		}, []);
 	}
 	chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, { urls: urlFilters }, ['blocking']);
-	chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, { urls: urlFilters }, ['responseHeaders']);
 }
 
 /**
@@ -1250,10 +1246,7 @@ function initializeEventListeners() {
 		]
 	}, ['requestHeaders', 'blocking']);
 
-	// Fires each time that an HTTP(S) response header is received
-	// chrome.webRequest.onHeadersReceived
-
-	// Add onBeforeRequest and onHeadersReceived listeners which are shared by Ghostery and Antitracking
+	// Add onBeforeRequest listeners which are shared by Ghostery and Antitracking
 	addCommonGhosteryAndAntitrackingListeners();
 
 	// Fires when a redirect is about to be executed