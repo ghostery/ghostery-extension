f33ba701531c09ad009cec8cb975c8242c7c4be9
Dominik Lubański <dominik.lubanski@gmail.com>
2024-04-17 10:31:38 +0200

    Update panel navigation items

--- extension-manifest-v3/src/pages/panel/views/navigation.js
+++ extension-manifest-v3/src/pages/panel/views/navigation.js
@@ -12,15 +12,13 @@
 import { html, msg, router, store } from 'hybrids';
 
 import Session from '/store/session.js';
-import { SIGNON_PAGE_URL } from '/utils/api.js';
 import { openTabWithUrl } from '/utils/tabs.js';
 
 import assets from '/pages/settings/assets/index.js';
 
 const MENU = [
-  {},
   {
-    icon: 'alert',
+    icon: 'report',
     label: msg`Report a broken page`,
     href: 'https://www.ghostery.com/support?utm_source=gbe',
   },
@@ -34,12 +32,36 @@ const MENU = [
     label: msg`Contact support`,
     href: 'https://www.ghostery.com/support?utm_source=gbe',
   },
-  {},
+  { header: msg`Ghostery settings` },
   {
-    icon: 'settings',
-    label: msg`Ghostery settings`,
-    href: chrome.runtime.getURL('/pages/settings/index.html'),
+    icon: 'shield-menu',
+    label: msg`Privacy protection`,
+    href: chrome.runtime.getURL(
+      '/pages/settings/index.html#@gh-settings-privacy',
+    ),
   },
+  {
+    icon: 'websites',
+    label: msg`Websites`,
+    href: chrome.runtime.getURL(
+      '/pages/settings/index.html#@gh-settings-websites',
+    ),
+  },
+  {
+    icon: 'wtm',
+    label: 'WhoTracks.Me',
+    href: chrome.runtime.getURL(
+      '/pages/settings/index.html#@gh-settings-whotracksme',
+    ),
+  },
+  {
+    icon: 'user',
+    label: msg`My Account`,
+    href: chrome.runtime.getURL(
+      '/pages/settings/index.html#@gh-settings-account',
+    ),
+  },
+  {},
   {
     icon: 'info-menu',
     label: msg`About`,
@@ -48,18 +70,21 @@ const MENU = [
 ];
 
 if (__PLATFORM__ !== 'safari') {
-  MENU.unshift({
-    icon: 'heart',
-    label: msg`Become a Contributor`,
-    href: 'https://www.ghostery.com/become-a-contributor?utm_source=gbe',
-  });
+  MENU.unshift(
+    {
+      icon: 'heart',
+      label: msg`Become a Contributor`,
+      href: 'https://www.ghostery.com/become-a-contributor?utm_source=gbe',
+    },
+    {},
+  );
 }
 
 export default {
   session: store(Session),
   content: ({ session }) => html`
     <template layout="grid grow">
-      <ui-panel-header layout="fixed top left width:full">
+      <ui-panel-header>
         Menu
         <ui-action slot="actions">
           <a href="${router.backUrl()}">
@@ -67,84 +92,76 @@ export default {
           </a>
         </ui-action>
       </ui-panel-header>
-      <div layout="column gap padding:bottom margin:top:8">
-        ${store.ready(session) &&
-        html`
-          <ui-text>
-            <a
-              href="${session.user
-                ? chrome.runtime.getURL(
-                    '/pages/settings/index.html#@gh-settings-account',
-                  )
-                : SIGNON_PAGE_URL}"
-              target="_blank"
-              layout="block padding margin:0:1"
-              onclick="${openTabWithUrl}"
-            >
-              <gh-panel-menu-item icon="user">
-                ${session.user
-                  ? html`
-                      <div>${session.name}</div>
-                      <ui-text color="gray-600">${session.email}</ui-text>
-                    `
-                  : html`Sign in`}
-              </gh-panel-menu-item>
-            </a>
-          </ui-text>
-          ${MENU.filter(
-            // Hide the "Become a Contributor" menu item if the user is already a contributor
-            (_, i) => i !== 0 || (i === 0 && !session.contributor),
-          ).map(({ icon, label, href }) =>
-            label
-              ? html`
-                  <ui-text>
-                    <a
-                      href="${href}"
-                      layout="block padding margin:0:1"
-                      onclick="${openTabWithUrl}"
+      <gh-panel-container>
+        <div layout="column gap:0.5 padding:1:0">
+          ${store.ready(session) &&
+          html`
+            ${MENU.filter(
+              // Hide the "Become a Contributor" menu item if the user is already a contributor
+              (_, i) =>
+                __PLATFORM__ === 'safari' || i > 1 || !session.contributor,
+            ).map(({ icon, label, href, header }) =>
+              label
+                ? html`
+                    <ui-text>
+                      <a
+                        href="${href}"
+                        layout="block padding margin:0:1"
+                        onclick="${openTabWithUrl}"
+                      >
+                        <gh-panel-menu-item icon="${icon}">
+                          ${label}
+                        </gh-panel-menu-item>
+                      </a>
+                    </ui-text>
+                  `
+                : html`
+                    <ui-line></ui-line>
+                    ${header &&
+                    html`<ui-text
+                      type="label-s"
+                      color="gray-500"
+                      layout="padding:1:1:0 margin:0:1"
                     >
-                      <gh-panel-menu-item icon="${icon}">
-                        ${label}
-                      </gh-panel-menu-item>
-                    </a>
-                  </ui-text>
-                `
-              : html`<ui-line></ui-line>`,
-          )}
-        `}
-        ${session.contributor &&
-        html`
-          <ui-action>
-            <a
-              href="${chrome.runtime.getURL(
-                '/pages/settings/index.html#@gh-settings-account',
-              )}"
-              onclick="${openTabWithUrl}"
-            >
-              <gh-panel-navigation-card
-                layout="row gap:1.5 items:center margin:1.5"
+                      ${header.toUpperCase()}
+                    </ui-text>`}
+                  `,
+            )}
+          `}
+          ${session.contributor &&
+          html`
+            <ui-action>
+              <a
+                href="${chrome.runtime.getURL(
+                  '/pages/settings/index.html#@gh-settings-account',
+                )}"
+                onclick="${openTabWithUrl}"
               >
-                <img
-                  src="${assets['contributor_badge']}"
-                  layout="size:12"
-                  alt="Contribution"
-                />
-                <div>
-                  <ui-text type="label-l">You are awesome!</ui-text>
-                  <ui-text
-                    type="body-s"
-                    color="gray-600"
-                    layout="width:::200px"
-                  >
-                    Thank you for your support in Ghostery's fight for a web
-                    where privacy is a basic human right!
-                  </ui-text>
-                </div>
-              </gh-panel-navigation-card>
-            </a>
-          </ui-action>
-        `}
-      </div>
+                <gh-panel-navigation-card
+                  layout="row gap:1.5 items:center margin:1.5"
+                >
+                  <img
+                    src="${assets['contributor_badge']}"
+                    layout="size:12"
+                    alt="Contribution"
+                  />
+                  <div>
+                    <ui-text type="label-l">You are awesome!</ui-text>
+                    <ui-text
+                      type="body-s"
+                      color="gray-600"
+                      layout="width:::200px"
+                    >
+                      Thank you for your support in Ghostery's fight for a web
+                      where privacy is a basic human right!
+                    </ui-text>
+                  </div>
+                </gh-panel-navigation-card>
+              </a>
+            </ui-action>
+          `}
+        </div>
+      </gh-panel-container>
     </template>
   `,
 };
