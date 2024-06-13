c52d6b5182bd7973988666a700352846cc7f1f64
Dominik Lubański <dominik.lubanski@gmail.com>
2024-06-13 10:48:26 +0200

    Minor fixes to release script:
    * Branch name to `release`
    * Fixes locales command
    * Adds build number to commit message

--- extension-manifest-v3/scripts/release.sh
+++ extension-manifest-v3/scripts/release.sh
@@ -5,8 +5,8 @@
 # Create release branch from latest main
 git checkout main
 git pull
-git checkout -b mv3-release
-git checkout mv3-release
+git checkout -b release
+git checkout release
 
 # Remove local node_modules if exists
 rm -rf ./node_modules
@@ -30,14 +30,12 @@ tx pull -f
 
 # Get version from package.json
 version=$(node -p "require('./package.json').version")
+# Get build number from xcode
+buildVersion=$(cat ./xcode/Ghostery.xcodeproj/project.pbxproj | sed -n -e 's/^.*CURRENT_PROJECT_VERSION = \([0-9]*\).*$/\1/p')
 
 # Commit changes
 git add ../package-lock.json .
-git commit -m "Release $version"
-
-
-# Clean dist
-rm -rf ./dist
+git commit -m "Release v$version ($buildVersion)"
 
 # Open Xcode
 xed xcode
