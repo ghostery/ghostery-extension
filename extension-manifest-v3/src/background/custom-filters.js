df3ef3951109f3d17a29af215e879d868879a71a
Dominik Lubański <dominik.lubanski@gmail.com>
2024-08-14 15:53:06 +0200

    Typo fix

--- extension-manifest-v3/src/background/custom-filters.js
+++ extension-manifest-v3/src/background/custom-filters.js
@@ -6,8 +6,9 @@
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
-w * file, You can obtain one at http://mozilla.org/MPL/2.0
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
+
 import { store } from 'hybrids';
 import {
   parseFilters,
