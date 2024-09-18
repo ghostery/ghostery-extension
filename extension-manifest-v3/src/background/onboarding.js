d3683fc176694041d65e4c88473a8740d1c04ddf
Dominik Lubański <dominik.lubanski@gmail.com>
2024-09-18 10:30:40 +0200

    Move opera notification to serp.js

--- extension-manifest-v3/src/background/onboarding.js
+++ extension-manifest-v3/src/background/onboarding.js
@@ -8,51 +8,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
+import { observe } from '/store/options.js';
 
-import { store } from 'hybrids';
-
-import Options, { observe } from '/store/options.js';
-
-import { isSerpSupported } from '/utils/opera.js';
-import { isOpera } from '/utils/browser-info.js';
-
-import { openNotification } from './notifications.js';
-
-let done = false;
-
-observe('onboarding', (onboarding) => {
-  done = onboarding.done;
-
-  if (!done && !onboarding.shownAt) {
+observe('onboarding', ({ done, shownAt }) => {
+  if (!done && !shownAt) {
     chrome.tabs.create({
       url: chrome.runtime.getURL('/pages/onboarding/index.html'),
     });
   }
 });
-
-if (__PLATFORM__ === 'chromium' && isOpera()) {
-  const NOTIFICATION_DELAY = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
-  const NOTIFICATION_SHOW_LIMIT = 4;
-
-  chrome.webNavigation.onCompleted.addListener(async (details) => {
-    if (done && details.frameId === 0) {
-      if (await isSerpSupported()) return;
-
-      const { onboarding } = await store.resolve(Options);
-
-      if (
-        // Onboarding is not "done"
-        !onboarding.done ||
-        // The notification was already shown maximum times
-        onboarding.serpShown >= NOTIFICATION_SHOW_LIMIT ||
-        // The notification was already shown recently
-        (onboarding.serpShownAt &&
-          Date.now() - onboarding.serpShownAt < NOTIFICATION_DELAY)
-      ) {
-        return false;
-      }
-
-      openNotification(details.tabId, 'opera-serp');
-    }
-  });
-}
