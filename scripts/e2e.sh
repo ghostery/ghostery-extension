d7c85c73364aef32b543d27ce8a0ad9dd1def302
Dominik Lubański <dominik.lubanski@gmail.com>
2024-10-03 12:03:59 +0200

    Minor fixes

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -2,11 +2,11 @@ rm -rf ./web-ext-artifacts
 mkdir ./web-ext-artifacts
 
 # Build and copy Chrome extension
-npm run build
+npm run build -- --silent
 cp -r ./dist ./web-ext-artifacts/ghostery-chromium
 
 # Build and pack Firefox extension
-npm run build firefox
+npm run build -- firefox --silent
 web-ext build --overwrite-dest -n ghostery-firefox.zip
 
 # Run e2e tests
