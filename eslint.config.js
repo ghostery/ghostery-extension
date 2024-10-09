c9a17670db25761f6b2f8a2ca0fc077129bae67c
Krzysztof Modras <chrmod@chrmod.net>
2024-10-09 16:09:30 +0200

    Fix prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
 import globals from 'globals';
 
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   js.configs.recommended,
-  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
