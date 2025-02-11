86bf98389a792362e68003027fdecd4a7c687f52
Dominik Lubański <dominik.lubanski@gmail.com>
2025-02-11 11:12:35 +0100

    fix(panel): broken page report - os metadata & optional email

--- src/background/broken-page-report.js
+++ src/background/broken-page-report.js
@@ -19,10 +19,10 @@ import { SUPPORT_PAGE_URL } from '/utils/urls.js';
 import { tabStats } from './stats.js';
 
 async function getMetadata(tab) {
-  let result = '\n------\n\n';
+  let result = '\n------\n';
 
   const { version } = chrome.runtime.getManifest();
-  result += `Extension version: ${version}`;
+  result += `v${version}\n`;
 
   // Send only not-private options
   const options = Object.fromEntries(
@@ -31,11 +31,11 @@ async function getMetadata(tab) {
     ),
   );
 
-  result += `\nOptions: ${JSON.stringify(options)}`;
+  result += `${JSON.stringify(options, null, 2)}\n\n`;
 
   const trackers = tabStats.get(tab.id)?.trackers.map((t) => t.id);
   if (trackers) {
-    result += `\nTrackers(${trackers.length}): ${trackers.join(', ')}`;
+    result += `Trackers(${trackers.length}): ${trackers.join(', ')}`;
   }
 
   return result;
@@ -48,8 +48,10 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         const formData = new FormData();
         const browserInfo = await getBrowserInfo();
 
-        formData.append('support_ticket[user_name]', msg.email);
-        formData.append('support_ticket[user_email]', msg.email);
+        const email = msg.email || 'noreplay@ghostery.com';
+
+        formData.append('support_ticket[user_name]', email);
+        formData.append('support_ticket[user_email]', email);
         formData.append(
           'support_ticket[subject]',
           `[GBE] Broken page report: ${msg.url}`,
@@ -58,10 +60,11 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         formData.append('support_ticket[selected_browser]', browserInfo.name);
         formData.append('support_ticket[browser_version]', browserInfo.version);
 
-        if (browserInfo.osVersion !== 'other') {
-          formData.append('support_ticket[selected_os]', browserInfo.osVersion);
-          formData.append('support_ticket[os_version]', '');
-        }
+        formData.append(
+          'support_ticket[selected_os]',
+          browserInfo.os !== 'other' ? browserInfo.os : 'other_os',
+        );
+        formData.append('support_ticket[os_version]', browserInfo.osVersion);
 
         let description = msg.description.trim() + (await getMetadata(msg.tab));
 
