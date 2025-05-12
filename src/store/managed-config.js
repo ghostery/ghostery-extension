3c9eedd53197cf9834bd0f54de2bdf5455154442
Dominik Lubański <dominik.lubanski@gmail.com>
2025-05-12 11:25:18 +0200

    fix(managed): use separate model for managed storage config (#2423)

---
+++ src/store/managed-config.js
@@ -0,0 +1,33 @@
+/**
+ * Ghostery Browser Extension
+ * https://www.ghostery.com/
+ *
+ * Copyright 2017-present Ghostery GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+import { store } from 'hybrids';
+
+import { isOpera } from '/utils/browser-info.js';
+
+const ManagedConfig = {
+  disableOnboarding: false,
+  disableUserControl: false,
+  disableUserAccount: false,
+  trustedDomains: [String],
+
+  [store.connect]: async () => {
+    if (__PLATFORM__ === 'safari' || isOpera()) return {};
+
+    try {
+      return (await chrome.storage.managed.get()) || {};
+    } catch {
+      return {};
+    }
+  },
+};
+
+export default ManagedConfig;
