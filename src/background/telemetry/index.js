3d78e5843199f2bff8081ff9d924ad5e24486a15
Krzysztof Modras <1228153+chrmod@users.noreply.github.com>
2025-10-10 14:34:56 +0200

    fix(telemetry): chrome store utm attribution (#2782)

--- src/background/telemetry/index.js
+++ src/background/telemetry/index.js
@@ -21,13 +21,15 @@ import Metrics, { processUrlQuery } from './metrics.js';
 async function detectUTMs() {
   const tabs = await chrome.tabs.query({
     url: [
-      'https://www.ghostery.com/*',
-      'https://www.ghosterystage.com/*',
+      'https://chromewebstore.google.com/detail/ghostery-*/mlomiejdfkolichcflejclcbmpeaniij*',
       'https://chrome.google.com/webstore/detail/ghostery-*/mlomiejdfkolichcflejclcbmpeaniij*',
       'https://microsoftedge.microsoft.com/addons/detail/ghostery-*/fclbdkbhjlgkbpfldjodgjncejkkjcme*',
       'https://addons.mozilla.org/*/firefox/addon/ghostery/*',
       'https://addons.opera.com/*/extensions/details/ghostery/*',
-      'https://apps.apple.com/app/apple-store/id1436953057/*',
+      'https://apps.apple.com/app/apple-store/id6504861501/*',
+      'https://apps.apple.com/*/app/ghostery-*/id6504861501*',
+      'https://www.ghostery.com/*',
+      'https://www.ghosterystage.com/*',
     ],
   });
 
