1d704467c2b8fb776f1adc2e0073d802053d5f5e
HoJeong Go <seia@outlook.kr>
2026-01-26 09:10:34 -0800

    fix: clean-up when we get a main frame request

--- src/background/adblocker/ancestors.js
+++ src/background/adblocker/ancestors.js
@@ -57,7 +57,16 @@ export class FramesHierarchy {
     let framesLength = frames.length;
     let frameIndex = framesLength;
 
-    if (documentId.length) {
+    // When the main frame refreshes, we assume that subframes
+    // to be refreshed as well. Therefore, we unlink the
+    // subframes from the list.
+    if (frameId === 0) {
+      for (const frame of frames) {
+        if (frame.parent === 0) {
+          this.unregister(tabId, frame.id);
+        }
+      }
+    } else if (documentId.length) {
       while (--frameIndex !== -1) {
         // If we found the frame having same `documentId` and
         // `frameId`, we can safely exit here.
@@ -66,8 +75,6 @@ export class FramesHierarchy {
           frames[frameIndex].parent = parentFrameId;
           frames[frameIndex].documentId = documentId;
           frames[frameIndex].details = details;
-
-          break;
         } else if (frames[frameIndex].documentId === documentId) {
           // If `frameId` doesn't match, it means the frame is
           // replacing the other frame.
