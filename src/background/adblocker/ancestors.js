908868815af614e56d7998c96b715477b1bae0c4
HoJeong Go <seia@outlook.kr>
2026-02-06 13:15:04 +0900

    fix: module path resolving

--- src/background/adblocker/ancestors.js
+++ src/background/adblocker/ancestors.js
@@ -9,7 +9,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
 
-import { parseWithCache } from '/utils/request';
+import { parseWithCache } from '/utils/request.js';
 
 export class FramesHierarchy {
   /**
