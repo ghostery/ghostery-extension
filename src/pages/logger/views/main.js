78f155a9a25df7f3ed3699fca9d82cb215e8ad72
Dominik Lubański <dominik.lubanski@gmail.com>
2026-02-13 11:15:30 +0100

    Update src/pages/logger/views/main.js
    Co-authored-by: Philipp Claßen <philipp@ghostery.com>

--- src/pages/logger/views/main.js
+++ src/pages/logger/views/main.js
@@ -48,7 +48,7 @@ async function downloadReport(host) {
   ]);
 
   await download({
-    data: stringify(report, { header: true, columns: REPORT_COLUMNS }),
+    data: stringify(report, { header: true, columns: REPORT_COLUMNS, escape_formulas: true }),
     filename: `ghostery-logger-report.csv`,
     type: 'text/csv;charset=utf-8;',
   });
